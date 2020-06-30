6081309df2b4676d970f8c6ff439c7cb68f24ee0
Raj Rahman <raj.rahman@outlook.com>
2020-06-30 22:22:11 +0100

    made call to Trello and retrieved list of things to do

--- session_items.py
+++ session_items.py
@@ -1,19 +1,61 @@
+import requests, json
 from flask import session
 
-_DEFAULT_ITEMS = [
+""" _DEFAULT_ITEMS = [
     { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
     { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
-]
+] """
 
+key = "f8a479ff06b7be92d7bd9d6644a35e82"
+token = "4aa46c18e2349cba537fa7169eb9a7733aa16917a12b69d86f44758d8c3faeb2"
+uridomain = "https://api.trello.com/1/"
+boardid = "J3hiTYRX"
+
+def callTrelloAPI(method,call,id):
+
+    if(method=="boards" and call=="lists"):
+        callmethodurl = "boards/" + id + "/lists"
+    elif(method=="lists" and call=="cards"):
+        callmethodurl = "lists/"+ id +"/cards"
+
+    requestUrl = uridomain + callmethodurl + "?key=" + key + "&token=" + token
+    response = requests.get(requestUrl)
+    jsonResponse = json.loads(response.text)
+    return jsonResponse
+
+def getTrelloBoards(boardid):  
+    return callTrelloAPI("boards","lists",boardid)
+
+def getListId(listofboards, cardname):
+    _cardName = cardname
+    for i in listofboards:
+            if i['name'] == _cardName:
+                # print(i['id'])
+                break
+    return i['id']
+
+def getCardsOnList(listId):
+    jsonResponse = callTrelloAPI("lists","cards",listId)
+    items = []
+
+    for i in jsonResponse:
+        print(i['id'] + " " + i['name']) 
+        
+        item = { 'id': i['id'], 'title': i['name'], 'status': 'Things To Do' }
+
+        # Add the item to the list
+        items.append(item)
+        session['items'] = items
+
+    return items
 
 def get_items():
-    """
-    Fetches all saved items from the session.
-
-    Returns:
-        list: The list of saved items.
-    """
-    return session.get('items', _DEFAULT_ITEMS)
+    "Fetch cards from Trello board"
+    listofboards = getTrelloBoards(boardid)
+    listId = getListId(listofboards,"Things To Do")
+    items = getCardsOnList(listId)
+    #print(listId)
+    return session.get('items', items)
 
 
 def get_item(id):
