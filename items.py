17d139e4350f1eb93bff910bbd097e8b46fc1774
Raj Rahman <raj.rahman@outlook.com>
2020-07-10 15:38:02 +0100

    moved keys to enviroment file

---
+++ items.py
@@ -0,0 +1,14 @@
+class Items:
+
+    #wheels = 2
+    
+    def __init__(self, id, title, status ):
+        self.id = id
+        self.title = title
+        self.status = status
+        # Start in first gear.
+        #self.gear = 1;
+
+    #def change_gear(self, gear):
+    list = []
+    list = { 'id': self.id , 'title': self.title, 'status': self.status }
--- session_items.py
+++ session_items.py
@@ -1,49 +1,26 @@
-import requests, json
+import requests, json, os
 from flask import session
 
-key = "f8a479ff06b7be92d7bd9d6644a35e82"
-token = "4aa46c18e2349cba537fa7169eb9a7733aa16917a12b69d86f44758d8c3faeb2"
+#key = "f8a479ff06b7be92d7bd9d6644a35e82"
+#token = "4aa46c18e2349cba537fa7169eb9a7733aa16917a12b69d86f44758d8c3faeb2"
+key = os.environ.get("key", "")
+token = os.environ.get("token", "")
 uridomain = "https://api.trello.com/1/"
 boardid = "J3hiTYRX"
 
-def callTrelloAPI(method,section,call,id,args):
-######################################################################################
-### REFACTOR THIS ALL TO THIS FORMAT requests.put(url, params={key: value}, args) ####
-### requests.put('https://httpbin.org / put', data ={'key':'value'})              ####
-######################################################################################
+def get_items():
+    #Get cards from "Things To Do" list on Trello
+    getListsOnBoards(boardid) # get the lists on a board
 
-    if(method=="get" and section=="boards" and call=="lists"):
-        callurl = "boards/" + id + "/lists?"
-    elif(method=="get" and section=="lists" and call=="cards"):
-        callurl = "lists/"+ id +"/cards?"
-    elif(method=="post" and section=="lists"and call=="cards"):
-        callurl = "lists/"+ id +"/cards?name=" + args
-    elif(method=="put" and section=="cards"and call==""):
-        callurl = "cards/"+ id +"?idList=" + args
-    elif(method=="delete" and section=="cards"and call==""):
-        callurl = "cards/"+ id +"?" + args
-
-    requestUrl = uridomain + callurl + "&key=" + key + "&token=" + token
-
-    if(method == "get"):
-        response = requests.get(requestUrl)
-    elif(method == "post"):
-        response = requests.post(requestUrl)
-    elif(method == "put"):
-        response = requests.put(requestUrl)
-    elif(method == "delete"):
-        response = requests.delete(requestUrl)
-    
-    try:
-        jsonResponse = json.loads(response.text)
-    except:
-        jsonResponse = ""
-        print("No JSON Data returned from Trello")
-
-    return jsonResponse
+    # store all ToDo card information from all available lists 
+    session['items'] = []
+    items = getCardsOnList(session.get('Things To Do'),"Things To Do")
+    items = getCardsOnList(session.get('Doing'),"Doing")
+    items = getCardsOnList(session.get('Done'),"Done")
+    return session.get('items', items)
 
 def getListsOnBoards(boardid):  
-    "Fetch lists from Trello board and store in session"
+    # Fetch lists from Trello board and store in session
     if session.get('lists') is not None:
         return session.get('lists')
     else:
@@ -62,8 +39,12 @@ def getListId(listofboards, cardname):
         session[cardname] = i['id']
         return i['id']
 
-def getCardsOnList(listId,status):
-    # Get cards on a List and add to a new items list to display in HTML
+def getCardsOnList(listId,list):
+
+    if listId is None:
+        listId = getListId(session.get('lists'),list) # get list ID for the List
+    
+        # Get ToDo cards on a List and add to a new items list to display in HTML
     listofcards = callTrelloAPI("get","lists","cards",listId,"")
     if session.get('items') is not None:
         items = session.get('items')
@@ -72,22 +53,13 @@ def getCardsOnList(listId,status):
 
     for i in listofcards:
         # Create item array
-        item = { 'id': i['id'], 'title': i['name'], 'status': status }
+        item = { 'id': i['id'], 'title': i['name'], 'status': list }
+        #item = new items(i['id'], i['name'], list)
         # Add the item to the list
         items.append(item)
         session['items'] = items
 
     return items
-
-def get_items():
-    #Get cards from "Things To Do" list on Trello
-    lists = getListsOnBoards(boardid)    
-    getListId(lists,"Things To Do")
-    session['items'] = []
-    items = getCardsOnList(session.get('Things To Do'),"Things To Do")
-    items = getCardsOnList(session.get('Doing'),"Doing")
-    items = getCardsOnList(session.get('Done'),"Done")
-    return session.get('items', items)
 
 
 def get_item(id):
@@ -145,3 +117,39 @@ def markAsDone(cardId):
     lists = getListsOnBoards(boardid)  
     getListId(lists,"Done")
     callTrelloAPI("put","cards","",cardId, session.get('Done'))
+
+def callTrelloAPI(method,section,call,id,args):
+######################################################################################
+### REFACTOR THIS ALL TO THIS FORMAT requests.put(url, params={key: value}, args) ####
+### requests.put('https://httpbin.org / put', data ={'key':'value'})              ####
+######################################################################################
+
+    if(method=="get" and section=="boards" and call=="lists"):
+        callurl = "boards/" + id + "/lists?"
+    elif(method=="get" and section=="lists" and call=="cards"):
+        callurl = "lists/"+ id +"/cards?"
+    elif(method=="post" and section=="lists"and call=="cards"):
+        callurl = "lists/"+ id +"/cards?name=" + args
+    elif(method=="put" and section=="cards"and call==""):
+        callurl = "cards/"+ id +"?idList=" + args
+    elif(method=="delete" and section=="cards"and call==""):
+        callurl = "cards/"+ id +"?" + args
+
+    requestUrl = uridomain + callurl + "&key=" + key + "&token=" + token
+
+    if(method == "get"):
+        response = requests.get(requestUrl)
+    elif(method == "post"):
+        response = requests.post(requestUrl)
+    elif(method == "put"):
+        response = requests.put(requestUrl)
+    elif(method == "delete"):
+        response = requests.delete(requestUrl)
+    
+    try:
+        jsonResponse = json.loads(response.text)
+    except:
+        jsonResponse = ""
+        print("No JSON Data returned from Trello")
+
+    return jsonResponse
