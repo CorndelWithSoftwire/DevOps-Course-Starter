5d52ef62ff3ec9cd88afe4d200da832a8b0935b0
Carl Jarvis <carl.jarvis1@aexp.com>
2020-07-14 15:09:17 +0100

    add new page to display individual tasks, add code to add new items to list

--- app.py
+++ app.py
@@ -7,7 +7,18 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return render_template('index.html', todos=session.get_items())
+    return render_template('index.html', items=session.get_items())
+
+
+@app.route('/item/<id>')
+def get_item(id):
+    return render_template('todoSingle.html', items=session.get_item(id))
+
+
+@app.route('/add_item', methods=['POST'])
+def create_item():
+    session.add_item(request.form['user_name'])
+    return render_template('index.html', items=session.get_items())
 
 
 if __name__ == '__main__':
