eb1861fbf5edaeea9c7dfd3723dd649dcfc06482
Noah-Vincenz Noeh <NoahVincenz.Noeh@aexp.com>
2020-07-15 16:14:48 +0100

    Added functionality for adding items.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from session_items import get_items, add_item, get_item, save_item
 import session_items as session
 
 app = Flask(__name__)
@@ -6,7 +7,20 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items=get_items())
+
+@app.route('/add', methods=['POST'])
+def add():
+    add_item(request.form.get('input_field'))
+    return redirect(url_for('index'))
+
+@app.route('/mark', methods=['POST'])
+def mark():
+    id = request.form.get('item_id')
+    item = get_item(id)
+    item.status = 'Completed'
+    save_item(item)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
--- session_items.py
+++ session_items.py
@@ -1,8 +1,9 @@
 from flask import session
 
 _DEFAULT_ITEMS = [
-    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
-    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+    { 'id': 1, 'status': 'Not Started', 'title': 'Clean the living room' },
+    { 'id': 2, 'status': 'Not Started', 'title': 'Book appointment with Physio' },
+    { 'id': 3, 'status': 'Not Started', 'title': 'Finish Module 1 of DevOps Course' }
 ]
 
 
