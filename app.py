ff67230c5d20b70f4889c4c0156894b8d328b60c
ajegundo <anarita.jegundodepaiva@aexp.com>
2020-07-16 15:45:13 +0100

    feature: show all items feature: add new item

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
-@app.route('/')
+@app.route('/items')
 def index():
-    return 'Hello World!'
+    items = session.get_items()
+    return render_template('index.html', items=items)
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    title = request.form['title']
+    item = session.add_item(title)
+    items = session.get_items()
+
+    return render_template('index.html', items=items)
 
 if __name__ == '__main__':
     app.run()
