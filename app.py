22ab86e5e9f5e501a3cb6134eef0ffeaf587d724
jnsha1 <jatin.n.shah@aexp.com>
2020-07-17 12:34:24 +0100

    Module 1 - setup todo list and add a new todo

--- app.py
+++ app.py
@@ -4,9 +4,20 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = session.get_items()
+    return render_template('/index.html', items = items)
+
+
+@app.route('/additem', methods=['POST'])
+def addItem():
+    todoItemTitle = request.form.get('newitem')
+    session.add_item(todoItemTitle)
+    items = session.get_items()
+    return render_template('/index.html', items = items)
+
 
 if __name__ == '__main__':
     app.run()
