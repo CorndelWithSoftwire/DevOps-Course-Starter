8ed5cb6460a7b8f31a92ac213eea8e8c7f0daec0
jnsha1 <jatin.n.shah@aexp.com>
2020-07-18 06:58:56 +0100

    Module 1 - added checkbox to mark completion - added delete - sorted list order

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config.from_object('flask_config.Config')
 
 
 @app.route('/')
 def index():
     items = session.get_items()
-    return render_template('/index.html', items = items)
+    sorteditems = sorted(items, key=lambda item: item['status'], reverse=True)
+    return render_template('/index.html', items = sorteditems)
 
 
 @app.route('/additem', methods=['POST'])
-def addItem():
+def additem():
     todoItemTitle = request.form.get('newitem')
     session.add_item(todoItemTitle)
     return index()
+
+
+@app.route('/deleteitem/<id>', methods=['POST'])
+def deleteitem(id):
+    session.delete_item(id)
+    return index()
+
+
+# TODO
+@app.route('/check/<id>', methods=['POST'])
+def checkitem(id):
+    # todoItemTitle = request.form.get('')
+    # session.add_item(todoItemTitle)
+    item = session.get_item(id)
+    item['status'] = 'Completed'
+    session.save_item(item)
+    return index()
+
 
 if __name__ == '__main__':
     app.run()
