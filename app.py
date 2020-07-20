090e768b6771db19bac7f125535ec66d54be6c36
Carl Jarvis <carl.jarvis1@aexp.com>
2020-07-20 12:08:41 +0100

    add functions to delete and sort items in app

--- app.py
+++ app.py
@@ -1,24 +1,54 @@
 from flask import Flask, render_template, request, redirect, url_for
+from operator import itemgetter
 import session_items as session
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
+global sortSwitch
+sortSwitch = True
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html', items=session.get_items())
+    if request.method == 'POST':
+        session.add_item(request.form['item_title'])
+
+    items = session.get_items()
+    global sortSwitch
+    if sortSwitch:
+        items = sorted(items, key=itemgetter('status'))
+    else:
+        items = sorted(items, key=itemgetter('status'), reverse=True)
+    return render_template('index.html', items=items)
 
 
-@app.route('/item/<id>')
+@app.route('/item/<id>', methods=['POST', 'GET'])
 def get_item(id):
+    if request.method == 'POST':
+        item = session.get_item(id)
+        item = session.create_new_item(item['id'], item['title'], 'Complete')
+
+        session.save_item(item)
     return render_template('todoSingle.html', items=session.get_item(id))
 
 
-@app.route('/add_item', methods=['POST'])
-def create_item():
-    session.add_item(request.form['user_name'])
-    return render_template('index.html', items=session.get_items())
+@app.route('/delete_item/<id>', methods=['POST'])
+def delete_item(id):
+    if request.method == 'POST':
+        item = session.get_item(id)
+
+        session.delete_item(item)
+    return redirect("/")
+
+
+@app.route('/sorted_items')
+def sort_item():
+    global sortSwitch
+    if sortSwitch:
+        sortSwitch = False
+    else:
+        sortSwitch = True
+    return redirect("/")
 
 
 if __name__ == '__main__':
