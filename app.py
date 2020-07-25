acc699c4885e204fabb1e4cc2524918486037daf
yomi.olutimehin <yomi.olutimehin@yahoo.com>
2020-07-25 14:20:38 +0100

    Managed to fix the typeError

--- app.py
+++ app.py
@@ -4,21 +4,20 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
-@app.route('/')
-def index():
-    return render_template('index.html', todos=session.get_items())
 
+@app.route('/')
+def get_items():
+    return render_template('index.html', todos=session.get_items())
 
 
 @app.route('/item/<id>')
 def get_item(id):
-
     item = session.get_item(id)
     return render_template('OneItemDisplay.html', item=item)
 
+
 @app.route('/add', methods=['POST'])
 def add_item():
-
     if request.method == "POST":
         session.add_item(request.form.get("title"))
     return redirect("/")
@@ -26,23 +25,25 @@ def add_item():
 
 @app.route('/save', methods=['POST'])
 def save_item(item):
-
-    existing_items = get_items()
-    updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
-
-    session['items'] = updated_items
+    if request.method == "POST":
+        existing_items = get_items()
+        updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in
+                         existing_items]
+        session['items'] = updated_items
 
     return item
 
-@app.route('/delete<id>', methods=['POST'])
-def delete_item(item):
 
+@app.route('/delete/<id>', methods=['POST'])
+def delete_item(item):
     if request.method == "POST":
         item = session.get_item(id)
 
-        session.delete(item)
-
-    return redirect("/")
+        item = session.delete(item)
+        # item = session.remove(item)
+    # return redirect("/")
+    # return render_template('delete.html', item=item)
+    return render_template('index.html', todos=session.get_items())
 
 
 if __name__ == '__main__':
