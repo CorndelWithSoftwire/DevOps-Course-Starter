5742a9210cc97f7cc32707c31657deea4a4eef97
gsfranc <george.s.francis@aexp.com>
2020-07-30 17:13:14 +0100

    Update the list via POST and display results.

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
-from session_items import get_items
+from session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
+    return renderIndex()
+
+@app.route('/listitem', methods=['POST'])
+def addListItem():
+    print("Adding Item!")
+    print('Title=' + request.form.get('textbox'))
+    add_item(request.form.get('textbox'))
+    return renderIndex()
+
+def renderIndex():
     full_list = get_items()
     list_of_titles = []
     for item in full_list:
