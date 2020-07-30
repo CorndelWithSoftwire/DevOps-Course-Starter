92137acdffe984e574d023bd22ce6d20299d55bd
gsfranc <george.s.francis@aexp.com>
2020-07-30 16:33:18 +0100

    Display the list of items

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
-import session_items as session
+from session_items import get_items
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html', list=items)
 
 if __name__ == '__main__':
     app.run()
