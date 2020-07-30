a20a100fc4efdc67923c15fca127d2aa56a3a9c0
Toby Rowe <tobyrowe@me.com>
2020-07-30 17:09:04 +0100

    update

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+#changed to the template for local host
+
 @app.route('/')
 def index():
-    return 'Hello World!'
-
+    return render_template('index.html')
+    
 if __name__ == '__main__':
     app.run()
+
