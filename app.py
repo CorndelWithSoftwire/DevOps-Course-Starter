dc0cdb59cf80e470376117be5babf53334034b2a
shekher2001 <skandwal2001@gmail.com>
2020-07-30 07:51:00 +0100

    Feature branch updaed with Exercise 1

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = session.get_items()
+    return render_template('index.html', items = items)
+
+@app.route('/add_Item', methods=['POST'])
+def add_Item():
+    item = request.form['title']
+    session.add_item(item)
+
+    return 'Added Successfully!'
 
 if __name__ == '__main__':
     app.run()
