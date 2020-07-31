b38f1a5303bb28a043aae5dc82d0917a4e905ad5
gsfranc <george.s.francis@aexp.com>
2020-07-31 15:39:59 +0100

    Display status on page, add button to mark done.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from session_items import get_items, add_item
+from session_items import get_items, get_item, add_item, save_item
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
@@ -15,12 +15,19 @@ def addListItem():
     add_item(request.form.get('textbox'))
     return renderIndex()
 
+@app.route('/completeditem', methods=['POST'])
+def updateListItem():
+    print("Updating Item!")
+    print(request.form.get('id'))
+    item_to_update = get_item(request.form.get('id'))
+    print(item_to_update)
+    item_to_update['status'] = 'Done!'
+    save_item(item_to_update)
+    return renderIndex()
+
 def renderIndex():
     full_list = get_items()
-    list_of_titles = []
-    for item in full_list:
-        list_of_titles.append(item['title'])
-    return render_template('index.html', list=list_of_titles)
+    return render_template('index.html', list=full_list)
 
 if __name__ == '__main__':
     app.run()
