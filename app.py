5047465643ec1484e854d5b6aeba20d25c4930de
Toby Rowe <tobyrowe@me.com>
2020-08-05 14:52:47 +0100

    update with project draft v1

--- app.py
+++ app.py
@@ -10,8 +10,15 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = session.get_items()
+
+    return render_template('index.html', todos = items)
     
-if __name__ == '__main__':
-    app.run()
+@app.route('/add-todo', methods=["POST"])
+def add_todo():
+    item = request.form.get('name')
+
+    session.add_item(item)
+
+    return redirect('/')
 
