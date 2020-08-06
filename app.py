c4e68024489d02d324053eda3dbeedbf2254cbae
aokpokpo <aniekan.okpokpo@aexp.com>
2020-08-06 23:57:40 +0100

    adding and displayinig todo items

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = session.get_items()
+    return render_template("index.html", items = items)
+
+
+@app.route('/', methods = ['POST'])
+def add_item():
+    item_title = request.form.get('title')
+    session.add_item(item_title)
+    items = session.get_items()
+    return render_template("index.html", items = items)
 
 if __name__ == '__main__':
     app.run()
