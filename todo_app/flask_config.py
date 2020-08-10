f6107904cee8301469c08b65eea9683a29b11556
Robert Owen <robert.owen@softwire.com>
2020-08-10 16:20:18 +0100

    update error message

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -5,4 +5,4 @@ class Config:
     """Base configuration variables."""
     SECRET_KEY = os.environ.get('SECRET_KEY')
     if not SECRET_KEY:
-        raise ValueError("No SECRET_KEY set for Flask application. Did you forget to run setup.sh?")
+        raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
