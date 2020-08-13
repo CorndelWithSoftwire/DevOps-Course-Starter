085acd99878aa637c7b59a4d0f23229060de3200
Rohan Raj <rohan.raj@aexp.com>
2020-08-13 13:15:55 +0100

    Add item form and Mark complete functionality

--- app.py
+++ app.py
@@ -17,5 +17,13 @@ def addit():
         session.add_item(request.form.get('new_item'))
         return redirect('/')
 
+@app.route('/complete')
+def completeit():
+    if request.method == 'GET':
+        item = session.get_item(request.args.get('item_id'))
+        item['status']='Complete'
+        session.save_item(item)
+        return redirect('/')
+
 if __name__ == '__main__':
     app.run()
