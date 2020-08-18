0acad0e0fdf79f0ec4c12350542285771e27d684
Louis Egbufor <louis.egbufor@capita.com>
2020-08-18 22:37:31 +0100

    Addition of CRUD to TODO list project

--- app.py
+++ app.py
@@ -5,8 +5,35 @@ app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def index():    
+    items = session.get_items()
+    return  render_template("index.html",todoitems=items)#'Hello World!'
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/', methods=['POST'])
+def add_item():   
+    strtitle = request.form.get('title')#request.form['title']  
+    if strtitle!='':
+        session.add_item(strtitle)
+    return redirect('/')
+    
+@app.route('/<id>')
+def complete_item(id):   
+    strlist=session.get_item(id)
+    strlist['status']="Completed"
+    session.save_item(strlist)    
+    return redirect('/')  
+
+@app.route('/remove/<id>')
+def delete_item(id):   
+    session.remove_item(id)           
+    return redirect('/')  
+
+@app.route('/clear')
+def reinitialize_list():
+    session.clear_items()
+    return redirect('/')  
+
+   
--- session_items.py
+++ session_items.py
@@ -1,4 +1,5 @@
 from flask import session
+from collections import OrderedDict
 
 _DEFAULT_ITEMS = [
     { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
@@ -13,7 +14,9 @@ def get_items():
     Returns:
         list: The list of saved items.
     """
-    return session.get('items', _DEFAULT_ITEMS)
+    _DEFAULT_ITEM= sorted(_DEFAULT_ITEMS,key=lambda d: (d['status'],d['title']))
+   
+    return session.get('items', _DEFAULT_ITEM)
 
 
 def get_item(id):
@@ -43,12 +46,13 @@ def add_item(title):
     items = get_items()
 
     # Determine the ID for the item based on that of the previously added item
-    id = items[-1]['id'] + 1 if items else 0
-
+    id = len(items) + 1 if items else 0
+    #print(len(items))
     item = { 'id': id, 'title': title, 'status': 'Not Started' }
 
     # Add the item to the list
     items.append(item)
+    items= sorted(items,key=lambda d: (d['status'],d['title']))
     session['items'] = items
 
     return item
@@ -63,7 +67,37 @@ def save_item(item):
     """
     existing_items = get_items()
     updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
-
+    updated_items= sorted(updated_items,key=lambda d: (d['status'],d['title']))
     session['items'] = updated_items
 
     return item
+
+def clear_items():
+    _DEFAULT_ITEMS_temp = [
+        { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
+        { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+    ]
+    
+    _DEFAULT_ITEMS_temp= sorted(_DEFAULT_ITEMS_temp,key=lambda d: (d['status'],d['title']))
+    session['items']=_DEFAULT_ITEMS_temp
+    return _DEFAULT_ITEMS_temp
+
+def remove_item(id):
+    """
+    removes an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
+
+    Args:
+        item: The item id to remove.
+    """
+    items = get_items()
+
+    item = get_item(id)
+
+    # Add the item to the list
+    items.remove(item)
+    items= sorted(items,key=lambda d: (d['status'],d['title']))
+    session['items'] = items
+
+
+    return item
+
