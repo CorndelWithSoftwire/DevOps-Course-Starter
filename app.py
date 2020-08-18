856fef791bb9896153d74333435b2e0453c7d4bf
Dave Rawlinson <drawlinson48@gmail.com>
2020-08-18 17:16:43 +0100

    Module 2 programming

--- app.py
+++ app.py
@@ -4,28 +4,27 @@ from flask import Flask, render_template, request, redirect, url_for
 import requests                     # Import the whole of requests
 import json
 import session_items as session
-from Package1 import Module2        # Secrets for example Trello tokens etc in here (local only)
+import os        # Secrets for example Trello tokens etc in here (local only)
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 
 #Set up variables we'll be using
 
-trellokey=Module2.key             # get the secret key
-trellotoken=Module2.token         # get the secret token
+trellokey=os.environ["key"]            # get the secret key
+trellotoken=os.environ["token"]         # get the secret token
 cardsurl = "https://api.trello.com/1/cards"      # I could put the bit apart from 'cards' in some kind of 'parenturl' variable, but haven't!
 acardsurl = "https://api.trello.com/1/cards/5f352898cdb592062bb71e23"                   # Used for hardcoded single card transfer from TO DO to DONE.
 boardurl = "https://api.trello.com/1/boards/5f3528983d4fb244aae9f934/cards"             # The board ID is not a secret!
 destinationlistid= "5f35289963b720014dd8a0ce"              # For the TO DO list and our destination for list transfer.  This is not a secret!  But is of course unique to me.  Found using postman API request,
 listid = "5f352898dc8a8c31a0a1e439"                         # TO BE DONE list id - necessary for POST API CALL to add new items
 donelistid = "5f3528981725711087e10339"                     # ACTUALLY DONE LIST ID
-# thislist= ["apple", "banana", "cherry", "pumpkin","whatever","whatever2"]                      # Temporary when I didn't know how to initialise a list of multiple entries
 
 # My code deals with a maximum of 100 cards.
 
-thislist=["Empty"] * 100                                                                 #Final list of ALL information in cards, nothing stripped out .. ready for ...
-superlist=["Empty"] * 100                                                                #Final list of actual text in cards, with rest of information stripped out .. ready for HTML
-
+thislist=[]                                                                #Final list of ALL information in cards, nothing stripped out .. ready for ...
+superlist=[]                                                               #Final list of actual text in cards, with rest of information stripped out .. ready for HTML
+#superlist=dict()
 @app.route('/', methods = ["GET","PUT"])
 def index():
     # print ("Main application successfully refreshed")
@@ -55,20 +54,10 @@ def index():
  #   thetype = type(card_data)
     #  print(the_list[1])                                 # Here for example is the first card details.  But still, this massive list is actually simply 1 element of a string, which is no use
                                                     # So now I need to think of some way of getting this single string into seperate elements manually
-    counter = 0
-    for D in the_list:                              # for each of the full cards in the list 
-	    # print(the_list[D])                          # For some totally bizarre reason this doesn't work (show each line) .. So a totally sloppy solution below involving COUNTER all over the place
-        nextCard=str((the_list[counter]))
-        # thislist[counter]=(nextCard[8:32])          # Well it's always in the same place and the same length.  Hey .. at least this line works!  Don't knock it!
-        thislist[counter]=(nextCard)
-        startofcardname=(thislist[counter].find("'name'"))     # We next have to find how long the card name is within the string.  Remember this just a string not a list so the only way of doing this, START
-        endofcardname=(thislist[counter].find(" 'pos'"))     # End
-        # print((thislist[counter])[startofcardname+9:endofcardname-2])         # Here for testing purposes
-        superlist[counter]=((thislist[counter])[startofcardname+9:endofcardname-2])
-        counter = counter +1                        # As said previously - this is really rubbish .. but 'D' won't work, and I'm running out of time .. so this is the only way forward.                  
-    # print(superlist)                              For testing purposes
+    for todo in the_list:
+        superlist.append(todo["name"])                              # for each of the full cards in the list 
 
-# I tried all the below but nothing would work in order to get those card id's into different parts of the list, hence instead had to use the above totally ugly solution
+# I tried all the below but literally nothing would work in order to get those card id's into different parts of the list, so next tried this:
  #   counter = 0
  #   for D in thislist:
  #                                                   # We already know the key and token .. so I think query is already good.  We just have to change the URL each hit.
@@ -97,23 +86,36 @@ def index():
 # ---------------------------------------------
 
 # CHANGE LIST THAT A CARD IS IN.   THE MVP FOR THE SPEC IS I MOVE ANY CARD FROM TO DO INTO DOING.  So this is triggered by execution (or web page refresh) and hard-code moves one of my tasks to a different list.
-# This PUT should be in a different method but I've spent so many hours already.
-# # 10 LINES TEMPORARILY REMOVED
- #   query = {
- #       'key': trellokey,
- #       'token': trellotoken,
- #       'idList': destinationlistid   
- #   }
- #   response2 = requests.request(
- #        "PUT",
- #        acardsurl,
- #        params=query
- #    )
+# This PUT should be in a different method altogether but I've spent so many hours already that I have to move forward somehow.
+# 
+    query = {
+        'key': trellokey,
+        'token': trellotoken,
+        'idList': destinationlistid   
+    }
+    response2 = requests.request(
+         "PUT",
+         acardsurl,
+         params=query
+     )
 
-    print(Items)
-    print(superlist)
+    # Desperate to try and turn the list into a dictionery so somehow I can get HTML to print it into different lines so I can add a button against them, but it just simply isn't going to work I can only get the whole list printed as a single line in the HTML which is of course useless.
 
-    return render_template('index.html',passedItems=Items,todisplay=superlist[1])
+    # During module_1 a dict was returned that was in a format that somehow HTML seems to deal with.  No matter what I try I can't get 'superlist' into a format that simulates the format of the cookies list that works on the HTML.  It's too hard.  I've spent 4 hours on it.  It seems impossible with the info we have. 
+    # I'm having to very deep dive the 'enumerate' and indices operations but I can't spend lots more hours in this.
+    #superlist2 = {key: [index for index, x in enumerate(superlist) if x == key] for key in superlist}.  All the below fails.
+    #my_dict={
+    #for key in superlist:
+    #    indices = []
+    #    for index,value in enumerate(superlist):
+    #        if value == key:
+    #            indices.append(index)
+    #    my_dict[key] = indices
+    #print(my_dict)
+
+    # print(superlist2)
+
+    return render_template('index.html',passedItems=Items,todisplay=superlist)
 
 @app.route('/addentry', methods = ["POST"])
 
