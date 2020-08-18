70d6c4d8c7742c1b36117e736839a669ba5217a5
Ian Martin <ian.martin3@capita.com>
2020-08-18 13:50:53 +0100

    Initial commit

--- app.py
+++ app.py
@@ -5,8 +5,21 @@ app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def index(name=None):
+    return render_template('index.html', items = session.get_items())
+
+@app.route('/', methods = ['POST', 'GET'])
+def addNew():
+    if request.method == 'POST':
+        NewItem = request.form["NewItem"]
+        session.add_item(NewItem)
+        return render_template('index.html', items = session.get_items())
+
+@app.route('/', methods = ['PUT', 'GET'])
+def markascomplete():
+    if request.method == 'PUT':
+        session.save_item(id)
+        return render_template('index.html', items = session.get_items())
 
 if __name__ == '__main__':
     app.run()
