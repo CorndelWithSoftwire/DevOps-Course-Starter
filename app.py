515087fda23b469ad8bedfede38a983ed6222963
Kevin Eaton <kevin.eaton@capita.com>
2020-08-18 11:55:56 +0100

    replaced hello world with template

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
