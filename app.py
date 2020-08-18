fee334e21adbcc8b7f1a5f1b589e5730d9a9d77b
shivaHanchate <69221323+shivaHanchate@users.noreply.github.com>
2020-08-18 23:32:39 +0100

    Add post method to TO DO App

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+
 @app.route('/')
-def index():
-    return 'Hello World!'
+def index(name=None):
+    return render_template('index.html', items=session.get_items())
+
+
+@app.route('/', methods=["POST"])
+def additem():
+    NewToDoItem = request.form["NewToDoItem"]
+    session.add_item(NewToDoItem)
+    return render_template('index.html', items=session.get_items())
+
 
 if __name__ == '__main__':
     app.run()
