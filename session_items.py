18cba858b5a432631451d665fe915ade6d1d2441
lindsey raven <lindseyraven6@gmail.com>
2020-08-18 16:52:55 +0100

    status update

--- session_items.py
+++ session_items.py
@@ -84,3 +84,31 @@ def delete_todo(todo_id):
 
 
     return todo_id
+
+
+def complete_todo(id):
+  
+    existing_items = get_items()
+
+    for item in range(len(existing_items)):
+        if existing_items[item]['id'] == int(id):
+            existing_items[item]['status'] = "Completed"
+            break
+
+    session['items'] = existing_items
+
+    return id
+
+
+def started_todo(id):
+  
+    existing_items = get_items()
+
+    for item in range(len(existing_items)):
+        if existing_items[item]['id'] == int(id):
+            existing_items[item]['status'] = "Started"
+            break
+
+    session['items'] = existing_items
+
+    return id
