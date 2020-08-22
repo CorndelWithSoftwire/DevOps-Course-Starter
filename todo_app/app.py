ec7ba601d35d3040278db4e88e09197ffa3eca37
Mark McEwen <mark.mcewen@capita.com>
2020-08-22 09:56:24 +0100

    Add functionality to remove an item.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from todo_app.flask_config import Config
 
-from todo_app.data.session_items import get_items, add_item, save_item, get_item
+from todo_app.data.session_items import get_items, add_item, save_item, get_item, remove_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -37,6 +37,11 @@ def sort_statutes():
     todos = get_items()
     newlist = sorted(todos, key=lambda k: k['status'])
     return render_template('index.html', todoList = newlist)
-    
+
+@app.route('/remove_todo/<id>', methods=['GET'])
+def remove_todo(id):
+    remove_item(id)
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run()
