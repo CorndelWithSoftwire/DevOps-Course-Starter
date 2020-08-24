4ad2d74dc42558b1e7425ff726769912f8a5422a
Kevin Eaton <kevin.eaton@capita.com>
2020-08-24 20:25:15 +0100

    Stretch Goal 1 - added toggle Item completed button

--- app.py
+++ app.py
@@ -11,6 +11,22 @@ def index():
     #pass the list into the index template for display
     return render_template('index.html', items=items)
 
+#route to allow updating of status for each item based on id number
+@app.route('/<id>', methods=['POST'])
+def item(id):
+    #populate variable with the relevant item
+    item = session.get_item(id)
+    #Check current status, update and then save accordingly
+    if item['status']=='Not Started':
+        item['status']='Complete'
+        session.save_item(item)
+    elif item['status']=='Complete':
+        item['status']='Not Started'
+        session.save_item(item)
+    #return user to index page
+    return redirect('/') 
+
+
 @app.route('/newitems', methods=['POST'])
 def newitems():
     #capture the title of newitem using a form and pass it to the add_item function
