50825f846e4338a5eb7fabcf1611de1c28f96803
Jamie Doyle <jamie.doyle@capita.com>
2020-08-28 13:15:50 +0100

    Added new route to add the output of form.  Completing standard goals.

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/', methods=['get'])
 def index():
-    # return 'Hello World!'
-    listall = session.get_items()
-    return render_template('index.html', listall=listall)
+    return render_template('index.html', listall=session.get_items())
+
+@app.route('/', methods=['post'])
+def add():
+    newItem = request.form.get('new_title')
+    session.add_item(newItem)
+
+    return render_template('index.html', listall=session.get_items())
 
 
 if __name__ == '__main__':
