869315e538b67e3f902138d45e4ac451c2dda89a
Jamie Doyle <jamie.doyle@capita.com>
2020-08-28 12:22:43 +0100

    Changed index.html and app.py to display the to-do list

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app.config.from_object('flask_config.Config')
 @app.route('/')
 def index():
     # return 'Hello World!'
-    return render_template('index.html')
+    listall = session.get_items()
+    return render_template('index.html', listall=listall)
 
 #if __name__ == '__main__':
 #    app.run()
