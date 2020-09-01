4b662cd91a4d17aa6d71fa7099cd5e3d64d149b2
IanRiley <69636250+IanRiley@users.noreply.github.com>
2020-09-01 20:31:41 +0100

    Core-ToDo
    just the core stuff

--- app.py
+++ app.py
@@ -6,7 +6,22 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    todo=session.get_items()
+    return render_template('index.html',all_todo=todo)
+
+@app.route('/', methods=["POST"])
+def add_todo():
+    td_new_title=request.form.get('td_Title')
+    if (len(td_new_title) > 1 ):
+        session.add_item(td_new_title)
+    return index()
+#    todo=session.get_items()
+#    return render_template('index.html',all_todo=todo)
+
+@app.route('/del', methods=['POST']) 
+def del_book():     
+# Code to create a new book entry in the database. 
+    return render_template('message.html')
 
 if __name__ == '__main__':
     app.run()
