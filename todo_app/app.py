4aa35ff41285992ebc4b9a0aad1110957242f640
Andy <andrew.want@capita.com>
2020-09-03 10:32:50 +0100

    3rd Sept - troubleshooting files

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flask import render_template
 from todo_app.flask_config import Config
+import session
+#import session
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 tasks = [{'id': 1, 'title': 'test'}, {'id': 2, 'title': 'testing'}, {'id': 3, 'title': 'tester'}]
 
-@app.route('/<id>')
+@app.route('/id')
 def index(id):
-    items = tasks
+    items = get_item(id)
     return render_template('Index.html', items=items)
 
 @app.route('/index')
