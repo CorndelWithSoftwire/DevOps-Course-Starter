4769b15b3e22d877d133f674a97636880ecdd589
aokpokpo <aniekan.okpokpo@aexp.com>
2020-09-04 00:18:01 +0100

    read and add items to trello

--- flask_config.py
+++ flask_config.py
@@ -4,5 +4,8 @@ import os
 class Config:
     """Base configuration variables."""
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    if not SECRET_KEY:
-        raise ValueError("No SECRET_KEY set for Flask application. Did you forget to run setup.sh?")
+    KEY = os.environ.get('KEY')
+    TOKEN = os.environ.get('TOKEN')
+    LIST_ID = os.environ.get('LIST_ID')
+    # if not SECRET_KEY:
+    #     raise ValueError("No SECRET_KEY set for Flask application. Did you forget to run setup.sh?")
