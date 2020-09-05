b623ade9845dfdef81e0078dc758d84866697bcb
Nick Chadwick <nick.chadwick@capita.com>
2020-09-05 10:04:19 +0100

    Main exercise completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, request, render_template
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, add_item
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,8 +9,15 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = get_items()
+    return render_template('index.html',items=items)
 
+@app.route('/add_todo_item', methods=['POST']) 
+def add_todo_item(): 
+    new_todo_item = request.form.get('newitem', "")
+    items = add_item(new_todo_item)
+    items = get_items()
+    return render_template('index.html',items=items)
 
 if __name__ == '__main__':
     app.run()
