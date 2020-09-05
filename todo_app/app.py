8960fb85a555477b5ac23c21c8df9fc04cbb7d8e
Nick Chadwick <nick.chadwick@capita.com>
2020-09-05 13:25:46 +0100

    Added a table for contents and order by status

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items, add_item
-
+from operator import itemgetter
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,14 +10,16 @@ app.config.from_object(Config)
 @app.route('/')
 def index():
     items = get_items()
-    return render_template('index.html',items=items)
+    ##renders the template with the items sorted by status
+    return render_template('index.html',items=sorted(items,key=itemgetter('status')))
 
 @app.route('/add_todo_item', methods=['POST']) 
 def add_todo_item(): 
     new_todo_item = request.form.get('newitem', "")
-    items = add_item(new_todo_item)
+    add_item(new_todo_item)
     items = get_items()
-    return render_template('index.html',items=items)
+    ##renders the template with the items sorted by status
+    return render_template('index.html',items=sorted(items,key=itemgetter('status')))
 
 if __name__ == '__main__':
     app.run()
