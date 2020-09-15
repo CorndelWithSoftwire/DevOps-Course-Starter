ddbec6fee56f1cdfe5d42bc606df723338bcf6a0
Haseeb Mahmood <Haseebmahmood00@gmail.com>
2020-09-15 16:30:58 +0100

    Improved formatting of items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, render_template, redirect
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,8 +8,16 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items_list = get_items()
+    return render_template("index.html", items = items_list)
+
+@app.route('/add_item')
+def add():
+    hardcoded_item_title = "New Item"
+    add_item(hardcoded_item_title)
+    return redirect("/")
 
 
 if __name__ == '__main__':
     app.run()
+
