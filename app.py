f38541c33ae11573c8de32fb8aefc3d27081ecf3
Kevin Eaton <kevin.eaton@capita.com>
2020-09-16 23:05:19 +0100

    replaced get item logic to pull from trello API

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
+import requests
+from secrets import KEY, TOKEN
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    #populate variable with the list of saved todo items
-    items = session.get_items()
-    #sort the list ased on the value of the status field in reverse order
+    apiurl = "https://api.trello.com/1/"
+    boardsurl = "boards/5f6076c34c5f48265943e31e/"
+    cardsurl = "cards/"
+    query = {'key' : KEY, 'token' : TOKEN}
+    cards = requests.get(apiurl+boardsurl+"cards", params=query)
+    cards_json = cards.json()
+
+    items = []
+    for card in cards_json:
+        trellocard = requests.get(apiurl+cardsurl+card['id']+"/list", params=query)
+        trellocard_json = trellocard.json()
+        if trellocard_json['name'] == 'Done':
+            cardstatus = 'Completed'
+        else :
+            cardstatus = trellocard_json['name'] 
+        items.append({"id" : card['id'], "status": cardstatus, "title": card['name']})
     items=sorted(items, key=lambda k: k['status'], reverse=True)
-    #pass the sorted list into the index template for display
     return render_template('index.html', items=items)
 
 #route to allow updating of status for each item based on id number
