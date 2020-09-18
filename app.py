2df56001c2b771f25fde2e1495ecb47ddb0fd6b8
Jamie Doyle <jamie.doyle@capita.com>
2020-09-18 16:13:04 +0100

    Added all three lists

--- app.py
+++ app.py
@@ -14,13 +14,21 @@ app.config.from_object('flask_config.Config')
 def index():
     list_todo_url = 'https://api.trello.com/1/lists/5f637aafcce13f603c570ebd/cards?key=' + str(os.getenv('TRELLO_KEY')) + "&token=" + str(os.getenv('TRELLO_TOKEN'))
     todo_list = requests.get(list_todo_url)
-    json_data = todo_list.json()
-    return render_template('index.html', listall=json_data)
+    todo_json = todo_list.json()
+
+    list_doing_url = 'https://api.trello.com/1/lists/5f637aaff47bd67c32c891e5/cards?key=' + str(os.getenv('TRELLO_KEY')) + "&token=" + str(os.getenv('TRELLO_TOKEN'))
+    doing_list = requests.get(list_doing_url)
+    doing_json = doing_list.json()    
+    
+    list_done_url = 'https://api.trello.com/1/lists/5f637aaf45a6967f43725861/cards?key=' + str(os.getenv('TRELLO_KEY')) + "&token=" + str(os.getenv('TRELLO_TOKEN'))
+    done_list = requests.get(list_done_url)
+    done_json = done_list.json()
+
+    return render_template('index.html', list_todo=todo_json, list_doing=doing_json, list_done=done_json)
 
 @app.route('/additem', methods=['post'])
 def add():
     newItem = request.form.get('new_title')
-    #session.add_item(newItem)
     add_todo_url = 'https://api.trello.com/1/cards?key=' + str(os.getenv('TRELLO_KEY')) + "&token=" + str(os.getenv('TRELLO_TOKEN')) + "&idList=5f637aafcce13f603c570ebd" + "&name=" + newItem + "&desc=from Jamies App"
     todo_list = requests.post(add_todo_url)
     return redirect('/', code=302)
