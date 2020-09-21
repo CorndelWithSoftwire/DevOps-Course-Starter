7d46c375d489a1b9d16a59d83eedceab0685cc7f
Mark McEwen <mark.mcewen@capita.com>
2020-09-21 15:30:56 +0100

    Credentials handling

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,31 +1,50 @@
 import requests
-
-trello_key = ''
-trello_token = ''
-
-def get_trello_secrets():
-    trello_secrets = []
-    with open("todo_app/trello_secrets.txt", 'r') as file:
-        split_lines = [line.split('=') for line in file.read().splitlines()]
-        trello_secrets = [{'k': k, 'v': v} for [k, v] in split_lines]
+import json
+class Trello_service(object):
+    TRELLO_API_URL = 'https://api.trello.com/1/'
+    TRELLO_BOARD_ID = '5f6456f8fc414517ed9b0e41'
+    TRELLO_CREDENTIALS = ''
     
-    for item in trello_secrets:
-        k = item['k']
-        v = item['v']
-        if k == "key":
-            trello_key = v
-        else:
-            trello_token = v
+    trello_key = ''
+    trello_token = ''
+    trello_lists = []
 
-    print( trello_key, trello_token)
+    def get_trello_secrets(self):
+        trello_secrets = []
+        with open("todo_app/trello_secrets.txt", 'r') as file:
+            split_lines = [line.split('=') for line in file.read().splitlines()]
+            trello_secrets = [{'k': k, 'v': v} for [k, v] in split_lines]
+        
+        for item in trello_secrets:
+            k = item['k']
+            v = item['v']
+            if k == "key":
+                self.trello_key = v
+            else:
+                self.trello_token = v
+        
+        self.TRELLO_CREDENTIALS = f"key={self.trello_key}&token={self.trello_token}"
 
-def get_items():
-    """
-    Fetches all saved items from the session.
+    def get_lists(self):
+        pass
 
-    Returns:
-        list: The list of saved items.
-    """
-    return session.get('items', _DEFAULT_ITEMS)
+    def get_items(self):
+        """
+        Fetches all saved items from the session.
 
-get_trello_secrets()
+        Returns:
+            list: The list of saved items.
+        """
+        url = f"{self.TRELLO_API_URL}boards/{self.TRELLO_BOARD_ID}/lists?{self.TRELLO_CREDENTIALS}"
+
+        payload = {}
+        headers = {}
+
+        response = requests.request("GET", url, headers=headers, data = payload)
+
+        lists = (json.loads(response.text.encode('utf8')))
+        #return session.get('items', _DEFAULT_ITEMS)
+
+service = Trello_service()
+service.get_trello_secrets() 
+service.get_items()
