b494edf28b570e665cbbd31c1cf44998967bebf7
Mark McEwen <mark.mcewen@capita.com>
2020-09-21 16:07:58 +0100

    get_items work

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -4,10 +4,11 @@ class Trello_service(object):
     TRELLO_API_URL = 'https://api.trello.com/1/'
     TRELLO_BOARD_ID = '5f6456f8fc414517ed9b0e41'
     TRELLO_CREDENTIALS = ''
-    
+
     trello_key = ''
     trello_token = ''
-    trello_lists = []
+    trello_lists = {}
+    items = []
 
     def get_trello_secrets(self):
         trello_secrets = []
@@ -24,9 +25,15 @@ class Trello_service(object):
                 self.trello_token = v
         
         self.TRELLO_CREDENTIALS = f"key={self.trello_key}&token={self.trello_token}"
+        self.get_lists()
 
     def get_lists(self):
-        pass
+        url = f"{self.TRELLO_API_URL}boards/{self.TRELLO_BOARD_ID}/lists?{self.TRELLO_CREDENTIALS}"
+        response = requests.request("GET", url)
+        raw_lists = (json.loads(response.text.encode('utf8')))
+        for trello_list in raw_lists:
+            trelloListDict = dict(name=trello_list['name'], boardId=trello_list['idBoard'])
+            self.trello_lists[trello_list['id']] = trelloListDict
 
     def get_items(self):
         """
@@ -35,16 +42,18 @@ class Trello_service(object):
         Returns:
             list: The list of saved items.
         """
-        url = f"{self.TRELLO_API_URL}boards/{self.TRELLO_BOARD_ID}/lists?{self.TRELLO_CREDENTIALS}"
-
-        payload = {}
-        headers = {}
-
-        response = requests.request("GET", url, headers=headers, data = payload)
-
-        lists = (json.loads(response.text.encode('utf8')))
+        url = f"{self.TRELLO_API_URL}boards/{self.TRELLO_BOARD_ID}/cards?{self.TRELLO_CREDENTIALS}"
+        response = requests.request("GET", url)
+        cards = json.loads(response.text.encode('utf8'))
+        i = 0
+        for card in cards:
+            trelloListDict = self.trello_lists[card['idList']]
+            itemDict = dict(id=card['id'], status=trelloListDict['name'], title=card['name'], listId=card['idList'] )
+            self.items.insert(i, itemDict)
+            i += 1
+        print(self.items)
         #return session.get('items', _DEFAULT_ITEMS)
 
 service = Trello_service()
-service.get_trello_secrets() 
-service.get_items()
+service.get_trello_secrets()
+service.get_items() 
