bccefac3a8676469479904089a9154801b345fd7
jnsha1 <jatin.n.shah@aexp.com>
2020-09-22 21:14:02 +0100

    Added integration test with mocked response

--- app.py
+++ app.py
@@ -5,62 +5,65 @@ import dateutil.parser
 
 from viewmodel import ViewModel
 
-app = Flask(__name__, static_url_path='/static')
-app.config.from_object('flask_config.Config')
+
+def createListWithStatus(status, itemList):
+    return [TodoItem(x['name'], status, x['id'], duedate=x['due'], last_modified=x['dateLastActivity']) for x in
+            itemList]
 
 
-# Jinja filters
-@app.template_filter()
-def format_datetime(value):
-    fromisoformat = dateutil.parser.parse(value)
-    return fromisoformat.strftime("%b %d")
+def create_app():
+    app = Flask(__name__, static_url_path='/static')
+    app.config.from_object('flask_config.Config')
 
+    # Jinja filters
+    @app.template_filter()
+    def format_datetime(value):
+        fromisoformat = dateutil.parser.parse(value)
+        return fromisoformat.strftime("%b %d")
 
-app.jinja_env.filters['due_date_filter'] = format_datetime
+    app.jinja_env.filters['due_date_filter'] = format_datetime
 
+    @app.route('/')
+    def index():
+        getTodoList = TrelloGetCards().fetchForList(TODO_LIST_ID)
+        toDoItems = createListWithStatus(NOT_STARTED, getTodoList)
 
-@app.route('/')
-def index():
-    getTodoList = TrelloGetCards().fetchForList(TODO_LIST_ID)
-    toDoItems = [TodoItem(x['name'], NOT_STARTED, x['id'], duedate=x['due'], last_modified=x['dateLastActivity']) for x in getTodoList]
+        getDoneList = TrelloGetCards().fetchForList(DONE_LIST_ID)
+        doneItems = createListWithStatus(COMPLETED, getDoneList)
 
-    getDoneList = TrelloGetCards().fetchForList(DONE_LIST_ID)
-    doneItems = [TodoItem(x['name'], COMPLETED, x['id'], duedate=x['due'], last_modified=x['dateLastActivity']) for x in getDoneList]
+        items = toDoItems + doneItems
+        sorteditems = sorted(items, key=lambda item: item.status, reverse=True)
 
-    items = toDoItems + doneItems
-    sorteditems = sorted(items, key=lambda item: item.status, reverse=True)
+        item_view_model = ViewModel(sorteditems)
+        return render_template('index.html', view_model=item_view_model)
 
-    item_view_model = ViewModel(sorteditems)
-    return render_template('index.html', view_model=item_view_model)
+    @app.route('/additem', methods=['POST'])
+    def additem():
+        title = request.form.get('newitem')
+        duedate = request.form.get('duedate')
 
+        TrelloAddCard(TODO_LIST_ID).add(TodoItem(title, NOT_STARTED, duedate=duedate))
 
-@app.route('/additem', methods=['POST'])
-def additem():
-    title = request.form.get('newitem')
-    duedate = request.form.get('duedate')
+        return redirect(request.referrer)
 
-    TrelloAddCard(TODO_LIST_ID).add(TodoItem(title, NOT_STARTED, duedate=duedate))
+    @app.route('/deleteitem/<id>', methods=['POST'])
+    def deleteitem(id):
+        TrelloDeleteCard().delete(id)
 
-    return redirect(request.referrer)
+        return redirect(request.referrer)
 
+    @app.route('/check/<id>', methods=['POST'])
+    def checkitem(id):
+        item = TrelloGetCards().fetchCard(id)
+        list = TODO_LIST_ID
+        if request.form.get(id):
+            list = DONE_LIST_ID
 
-@app.route('/deleteitem/<id>', methods=['POST'])
-def deleteitem(id):
-    TrelloDeleteCard().delete(id)
+        TrelloUpdateCard().update(item, list)
+        return redirect(request.referrer)
 
-    return redirect(request.referrer)
-
-
-@app.route('/check/<id>', methods=['POST'])
-def checkitem(id):
-    item = TrelloGetCards().fetchCard(id)
-    list = TODO_LIST_ID
-    if request.form.get(id):
-        list = DONE_LIST_ID
-
-    TrelloUpdateCard().update(item, list)
-    return redirect(request.referrer)
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
---
+++ tests/test_app.py
@@ -0,0 +1,94 @@
+from unittest import mock
+from unittest.mock import patch
+
+import pytest
+from dotenv import load_dotenv, find_dotenv
+
+import app
+
+
+# def _mock_response(
+#         status=200,
+#         content="CONTENT",
+#         json_data=None,
+#         raise_for_status=None):
+#     mock_resp = mock.Mock()
+#     # mock raise_for_status call w/optional error
+#     mock_resp.raise_for_status = mock.Mock()
+#     if raise_for_status:
+#         mock_resp.raise_for_status.side_effect = raise_for_status
+#     # set status code and content
+#     mock_resp.status_code = status
+#     mock_resp.content = content
+#     # add json data if provided
+#     if json_data:
+#         mock_resp.json = mock.Mock(
+#             return_value=json_data
+#         )
+#     return mock_resp
+
+def _mock_response(
+        status=200,
+        content="CONTENT",
+        json_data=None,
+        raise_for_status=None):
+
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+        def raise_for_status(self):
+            return True
+
+    return MockResponse(json_data=json_data, status_code=status)
+
+
+@pytest.fixture
+def client():
+    # Use our test integration config instead of the 'real' version file_path = find_dotenv('.env.test')
+    file_path = find_dotenv('.env.test')
+    load_dotenv(file_path, override=True)
+    # Create the new app.
+    test_app = app.create_app()
+    # Use the app to create a test_client that can be used in our tests.
+    with test_app.test_client() as client:
+        yield client
+
+
+@patch('trello_request.requests.request')
+def test_index_page_with_todos(mock_get_requests, client):
+    todos_json = [
+        {
+            "id": "5f57f133fe6fec1ff7e25d63",
+            "dateLastActivity": "2020-09-21T17:39:25.603Z",
+            "name": "Valid Item",
+            "due": None
+        },
+        {
+            "id": "5f57f133fe6fec1ff7e25d64",
+            "dateLastActivity": "2020-09-21T17:39:25.603Z",
+            "name": "Some other Item",
+            "due": None
+        },
+        {
+            "id": "5f57f133fe6fec1ff7e25d65",
+            "dateLastActivity": "2020-09-21T17:39:25.603Z",
+            "name": "An Item with a due",
+            "due": "2020-09-22T10:00:00.603Z"
+        }
+    ]
+    mock_resp = _mock_response(json_data=todos_json)
+    mock_get_requests.return_value = mock_resp
+    response = client.get('/')
+
+    assert b"Valid Item" in response.data
+    assert b"Some other Item" in response.data
+    assert b"An Item with a due" in response.data
+    assert b"Sep 22" in response.data
+
+
+
