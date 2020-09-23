dabc212b6ecaf50b5ab7de4e2910e70cac54681b
Andy <andrew.want@capita.com>
2020-09-23 10:27:18 +0100

    'Working Env after folder restore (requests import issue into Poetry)

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
-from flask import request
+from flask import request,redirect,url_for
 from todo_app.flask_config import Config
 from flask import render_template
 from todo_app.data import session_items
+import requests
+import os
+import json
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-@app.route('/todo')
+def get_trello_API_credentials():
+    f = open("todo_app/Trello_API_Keys.txt", "r").read().split("\n")
+    return {'api_key': f[0], 'api_token': f[1]}
+
+
+@app.route('/')
 def index():
     items = session_items.get_items()
     return render_template('Index.html', items=items)
@@ -17,12 +25,12 @@ def task(id):
     item = session_items.get_item(id)
     return render_template('single_item.html', item=item)
 
-@app.route('/add', methods=['POST', 'GET'])
+@app.route('/add', methods=['POST'])
 def add():
     if request.method == 'POST':
         title = (request.form['title'])
         session_items.add_item(title)
-        return index()
+        return redirect(url_for('index'))
     else:
         return render_template('add_items.html')
 
