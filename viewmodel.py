4a1ba0860d5c51aabd488de52d01aedcba4111da
okpokpoaniekan@gmail.com <aniekan.okpokpo@aexp.com>
2020-09-26 19:31:10 +0100

    adding methods and test to filter items

--- viewmodel.py
+++ viewmodel.py
@@ -1,3 +1,6 @@
+from item import Item
+
+
 class ViewModel:
     def __init__(self, items):
         self._items = items
@@ -5,3 +8,13 @@ class ViewModel:
     @property
     def items(self):
         return self._items
+
+    @property
+    def todo_items(self):
+        todo_items = list(filter(lambda item: item.status == Item.STATUS_NOT_STARTED, self._items))
+        return todo_items
+
+    @property
+    def done_items(self):
+        done_items = list(filter(lambda item: item.status == Item.STATUS_COMPLETED, self._items))
+        return done_items
