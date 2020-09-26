d39530b3e6882a309181551d28334a8235553d97
okpokpoaniekan@gmail.com <aniekan.okpokpo@aexp.com>
2020-09-26 20:32:53 +0100

    Revert "adding methods and test to filter items"
    This reverts commit 4a1ba086

--- viewmodel.py
+++ viewmodel.py
@@ -1,6 +1,3 @@
-from item import Item
-
-
 class ViewModel:
     def __init__(self, items):
         self._items = items
@@ -8,13 +5,3 @@ class ViewModel:
     @property
     def items(self):
         return self._items
-
-    @property
-    def todo_items(self):
-        todo_items = list(filter(lambda item: item.status == Item.STATUS_NOT_STARTED, self._items))
-        return todo_items
-
-    @property
-    def done_items(self):
-        done_items = list(filter(lambda item: item.status == Item.STATUS_COMPLETED, self._items))
-        return done_items
