11d4d6150d1a3813e924c2326de42bdcbbca475c
okpokpoaniekan@gmail.com <aniekan.okpokpo@aexp.com>
2020-09-27 23:09:21 +0100

    reverse change

--- flask_config.py
+++ flask_config.py
@@ -8,5 +8,5 @@ class Config:
     TOKEN = os.environ.get('TOKEN')
     LIST_ID = os.environ.get('LIST_ID')
     DONE_LIST_ID = os.environ.get('DONE_LIST_ID')
-    # if not SECRET_KEY:
-    #     raise ValueError("No SECRET_KEY set for Flask application. Did you forget to run setup.sh?")
+    if not SECRET_KEY:
+        raise ValueError("No SECRET_KEY set for Flask application. Did you forget to run setup.sh?")
