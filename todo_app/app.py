29596dffc75aaefcfe6e7b3a641dec3d8a99aee8
Jack Welsh <jackrobertwelsh@gmail.com>
2020-10-05 17:21:30 +0100

    S95R-M2-ProjectExercise-DA6G

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,15 +1,50 @@
 from flask import Flask
+from flask import render_template,redirect,request
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import *
 
 app = Flask(__name__)
 app.config.from_object(Config)
+trello = Trello(Config)
 
 
+# web pages
 @app.route('/')
 def index():
-    return 'Hello World!'
+    lists = [List(k,i) for i,k in enumerate(trello.get_lists())]
+    cards = [Card(lists,i) for i in trello.get_cards()]
+    return render_template('index.html',cards=cards)
 
+@app.route('/card/<id>')
+def getitem(id):
+    lists = [List(k,i) for i,k in enumerate(trello.get_lists())]
+    card = Card(lists,trello.get_card(id))
+    return render_template('editCard.html',card=card,lists=lists)
+
+# post methods
+@app.route('/card/newcard',methods=["POST"])
+def newitem():
+    lists = [List(k,i) for i,k in enumerate(trello.get_lists())]
+    name = request.form['name']
+    desc = request.form['desc']
+    due = request.form['due']
+    trello.add_card(lists,name,desc,due)
+    return redirect('/')
+
+@app.route('/card/<id>/editcard',methods=["POST"])
+def editcard(id):
+    name = request.form['name']
+    desc = request.form['desc']
+    due = request.form['due']
+    idList = request.form['statusRadio']
+    trello.save_card(id,name,desc,due,idList)
+    return redirect('/')
+
+@app.route('/card/<id>/deletecard',methods=["POST"])
+def deleteitem(id):
+    trello.delete_card(id)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -1,69 +1,159 @@
 from flask import session
+import requests
+from requests.exceptions import HTTPError
+from datetime import datetime
+import json
 
-_DEFAULT_ITEMS = [
-    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
-    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
-]
 
+from todo_app.flask_config import Config
 
-def get_items():
+class Trello:
+    def __init__(self,Config):
+        self.board = Config.TRELLO_BOARD
+        self.url_prefix = "https://api.trello.com/1/"
+        self.query={
+            'key':Config.TRELLO_KEY,
+            'token':Config.TRELLO_TOKEN
+        }
+
+    def get_lists(self):
+        """
+        Gets the lists associated with the board
+
+        Returns:
+            dictionary of lists
+        """
+        url = self.url_prefix+"boards/"+self.board+"/lists"
+        response = requests.request("GET",url,params=self.query)
+        response.raise_for_status()
+        return response.json()
+    
+    def get_cards(self):
+        """
+        Gets the cards associated with the board
+
+        Returns:
+            dictionary of the cards on the board
+        """
+        url = self.url_prefix+"boards/"+self.board+"/cards"
+        response = requests.request("GET",url,params=self.query)
+        response.raise_for_status()
+        return response.json()
+
+    def get_card(self,id):
+        """
+        Fetches the saved item with the specified ID.
+
+        Args:
+            id: The ID of the card.
+
+        Returns:
+            card: The saved card
+        """
+        url = self.url_prefix+"cards/"+id
+        response = requests.request("GET",url,params=self.query)
+        response.raise_for_status()
+        return response.json()
+
+    def add_card(self,Lists,name,desc,due):
+        """
+        Adds a new item with the specified title to the session.
+
+        Args:
+            title: The title of the item.
+
+        Returns:
+            item: The saved item.
+        """
+        url = self.url_prefix+"cards"
+        query = self.query
+        query["name"]=name
+        query["idList"]=Lists[0].id
+        query["due"]=due
+        query["desc"]=desc
+        response = requests.request("POST",url,params=query)
+        response.raise_for_status()
+
+    def save_card(self,id,name,desc,due,idList):
+        """
+        Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
+
+        Args:
+            item: The item to save.
+        """
+        url = self.url_prefix+"cards/"+id
+        query = self.query
+        query["name"]=name
+        query["idList"]=idList
+        query["desc"]=desc
+        query["due"]=due
+        response = requests.request("PUT",url,params=query)
+        response.raise_for_status()
+
+    def delete_card(self,id):
+        """
+        Deletes an existing item in the session.
+
+        Args:
+            item: the item to delete
+        """
+        url = self.url_prefix+"cards/"+id
+        response = requests.request("DELETE",url,params=self.query)
+        response.raise_for_status()
+
+class Card:
     """
-    Fetches all saved items from the session.
+    card class for processing contents of API
 
     Returns:
-        list: The list of saved items.
+        Card class object
     """
-    return session.get('items', _DEFAULT_ITEMS)
+    def __init__(self,Lists,i):
+        self.id = i["id"]
+        self.name = i["name"]
+        self.listId = i["idList"]
+        self.desc = i["desc"]
+        self.creationTime = datetime.fromtimestamp(int(i["id"][0:8],16))
+        try:
+            self.due = datetime.strftime(datetime.fromisoformat(i["due"].replace('Z', '+00:00')),"%Y-%m-%d")
+        except:
+            self.due = ""
+        self.listName = [l.name for l in Lists if l.id == i["idList"]][0]
+        self.orderBy = [l.orderBy for l in Lists if l.id == i["idList"]][0]
 
+    def __repr__(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+            'listId':self.listId,
+            'desc':self.desc,
+            'creationTime':self.creationTime,
+            'due':self.due,
+            'list':self.listName,
+            'orderBy':self.orderBy
+            }
 
-def get_item(id):
+    def __str__(self):
+        return "Card(id="+self.id+", name="+self.name+")"
+
+class List:
     """
-    Fetches the saved item with the specified ID.
+    list class for processing contents of the API
 
-    Args:
-        id: The ID of the item.
-
-    Returns:
-        item: The saved item, or None if no items match the specified ID.
+    Return:
+        List class object
     """
-    items = get_items()
-    return next((item for item in items if item['id'] == int(id)), None)
+    def __init__(self,l,i):
+        self.id = l["id"]
+        self.name = l["name"]
+        self.orderBy = i
 
+    def __repr__(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+            'orderBy':self.orderBy
+            }
 
-def add_item(title):
-    """
-    Adds a new item with the specified title to the session.
-
-    Args:
-        title: The title of the item.
-
-    Returns:
-        item: The saved item.
-    """
-    items = get_items()
-
-    # Determine the ID for the item based on that of the previously added item
-    id = items[-1]['id'] + 1 if items else 0
-
-    item = { 'id': id, 'title': title, 'status': 'Not Started' }
-
-    # Add the item to the list
-    items.append(item)
-    session['items'] = items
-
-    return item
-
-
-def save_item(item):
-    """
-    Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
-
-    Args:
-        item: The item to save.
-    """
-    existing_items = get_items()
-    updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
-
-    session['items'] = updated_items
-
-    return item
+    def __str__(self):
+        return "List(id="+self.id+", name="+self.name+", orderBy:"+self.orderBy+")"
