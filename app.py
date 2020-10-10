db0a849464a1680f6477f59c9fce7d845ca75050
JamieDoyle74Cap <jamie.doyle@capita.com>
2020-10-10 11:50:33 +0100

    [exercise-2-jpd] created move_card_to_list method

--- app.py
+++ app.py
@@ -2,30 +2,40 @@ import os
 from dotenv import load_dotenv
 import requests
 from flask import Flask, render_template, request, redirect, url_for
-load_dotenv()
 
 class Todo:
     def __init__(self, item_id, name, status):
         self.item_id = item_id
         self.name = name
         self.status = status
-
+        
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+def get_trello_auth():
+    return {'key': os.getenv('TRELLO_KEY'), 'token': os.getenv('TRELLO_TOKEN')}
+
+def move_card_to_list(card_id, new_list_id):
+    params = get_trello_auth()
+    params['idList'] = new_list_id
+    return requests.put(f"https://api.trello.com/1/cards/{card_id}", params=params)
+
+
 @app.route('/', methods=['get'])
 def index():
-    todo_list_api_response_in_json = requests.get('https://api.trello.com/1/lists/' + os.getenv('TRELLO_TODO') + '/cards?key=' + os.getenv('TRELLO_KEY') + "&token=" + os.getenv('TRELLO_TOKEN')).json()
+
+    todo_list_api_response_in_json = requests.get('https://api.trello.com/1/lists/' + os.getenv('TRELLO_TODO') + '/cards', params=get_trello_auth()).json()
+    
     class_todo_list_api_response = []
     for iteminjson in todo_list_api_response_in_json:
         class_todo_list_api_response.append(Todo(iteminjson['id'],iteminjson['name'], 'To Do'))
 
-    doing_list_api_response_in_json = requests.get('https://api.trello.com/1/lists/' + os.getenv('TRELLO_DOING') + '/cards?key=' + os.getenv('TRELLO_KEY') + "&token=" + os.getenv('TRELLO_TOKEN')).json()
+    doing_list_api_response_in_json = requests.get('https://api.trello.com/1/lists/' + os.getenv('TRELLO_DOING') + '/cards', params=get_trello_auth()).json()
     class_doing_list_api_response = []
     for iteminjson in doing_list_api_response_in_json:
         class_doing_list_api_response.append(Todo(iteminjson['id'],iteminjson['name'], 'Doing'))
     
-    done_list_api_response_in_json = requests.get('https://api.trello.com/1/lists/' + os.getenv('TRELLO_DONE') + '/cards?key=' + os.getenv('TRELLO_KEY') + "&token=" + os.getenv('TRELLO_TOKEN')).json()
+    done_list_api_response_in_json = requests.get('https://api.trello.com/1/lists/' + os.getenv('TRELLO_DONE') + '/cards', params=get_trello_auth()).json()
     class_done_list_api_response = []
     for iteminjson in done_list_api_response_in_json:
         class_done_list_api_response.append(Todo(iteminjson['id'],iteminjson['name'], 'Done'))
@@ -39,20 +49,21 @@ def add():
     todo_list_api_response = requests.post('https://api.trello.com/1/cards?key=' + str(os.getenv('TRELLO_KEY')) + "&token=" + str(os.getenv('TRELLO_TOKEN')) + "&idList=5f637aafcce13f603c570ebd" + "&name=" + new_item + "&desc=from Jamies App")
     return redirect('/', code=302)
 
-@app.route('/movetodone/<id>', methods=['get'])
-def move_to_done(id):
-    move_list = requests.put('https://api.trello.com/1/cards/' + id +'?key=' + str(os.getenv('TRELLO_KEY')) + "&token=" + str(os.getenv('TRELLO_TOKEN')) + "&idList=5f637aaf45a6967f43725861")
-    return redirect('/', code=302)
-
 @app.route('/movetonew/<id>', methods=['get'])
 def move_to_new(id):
-    move_list = requests.put('https://api.trello.com/1/cards/' + id +'?key=' + str(os.getenv('TRELLO_KEY')) + "&token=" + str(os.getenv('TRELLO_TOKEN')) + "&idList=5f637aafcce13f603c570ebd")
+    move_card_to_list(id, os.getenv('TRELLO_TODO'))
     return redirect('/', code=302)
 
 @app.route('/movetodoing/<id>', methods=['get'])
 def move_to_doing(id):
-    move_list = requests.put('https://api.trello.com/1/cards/' + id +'?key=' + str(os.getenv('TRELLO_KEY')) + "&token=" + str(os.getenv('TRELLO_TOKEN')) + "&idList=5f637aaff47bd67c32c891e5")
+    move_card_to_list(id, os.getenv('TRELLO_DOING'))
     return redirect('/', code=302)
+
+@app.route('/movetodone/<id>', methods=['get'])
+def move_to_done(id):
+    move_card_to_list(id, os.getenv('TRELLO_DONE'))
+    return redirect('/', code=302)
+
 
 if __name__ == '__main__':
     app.run()
