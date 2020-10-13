c4e2b3994a33525fe8dfde62a932b17f4b19fe63
jeremy brex <jeremybrex@gmail.com>
2020-10-13 21:45:32 +0100

    module_1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,21 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, url_for
+import data.session_items as session
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'Hello World!'
+    if request.method == 'POST':
+        item_title = request.form['Title']
+        session.add_item(item_title)
+        return redirect('/')
+    else:
+        items_list = session.get_items()
+        # print(items_list)
+        return render_template('index.html', items_list=items_list)
 
 
 if __name__ == '__main__':
