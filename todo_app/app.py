ac903cf3819d712d2d7aca34c0f17f9b9711bd5d
JP <kjnvarma@gmail.com>
2020-10-14 12:50:49 +0100

    Project Exercises Module 1 Part 3

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,9 +1,15 @@
 import os.path
-from flask import Flask,escape,request,Response,render_template
+from flask import Flask,escape,request,Response,render_template, redirect
 import todo_app.data.session_items as session
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
+
+@app.route('/add_item')
+def add_item():
+    title = request.args.get('name')
+    session.add_item(title)
+    return redirect("/")
 
 @app.route('/')
 def index():
