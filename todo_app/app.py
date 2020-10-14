a28fae176749286ac780033e5a9827d745e2d322
JP <kjnvarma@gmail.com>
2020-10-14 12:34:11 +0100

    Project Exercises 1 Part 2

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,15 +1,16 @@
-from flask import Flask
-
-from todo_app.flask_config import Config
+import os.path
+from flask import Flask,escape,request,Response,render_template
+import todo_app.data.session_items as session
 
 app = Flask(__name__)
-app.config.from_object(Config)
-
+app.secret_key = "super secret key"
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = session.get_items()
+    return render_template("./index.html", items=items)
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
