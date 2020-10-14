3024d7798248d1eba28f3678df01065140b9020a
gm <bobinaki@gmail.com>
2020-10-14 15:23:15 +0100

    Added render_index_response function to remove duplication

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,29 +5,30 @@ from todo_app.data.session_items import *
 app = Flask(__name__)
 app.config.from_object(Config)
 
+def render_index_response():
+    return render_template('index.html', items = get_items())
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
-    return render_template('index.html', items = get_items())
+    return render_index_response()    
 
 @app.route('/add', methods=['POST'])
 def add():
     add_item(request.form.get('title'))
-    return render_template('index.html', items = get_items())
+    return render_index_response()
 
 @app.route('/setstatus/<id>/<status>', methods=['POST'])
 def setstatus(id,status):
     item = get_item(id)
     item['status'] = status
     save_item(item)
-    return render_template('index.html', items = get_items())
+    return render_index_response()
 
 @app.route('/delete/<id>', methods=['POST'])
 def delete(id):
     item = get_item(id)
     delete_item(item)
-    return render_template('index.html', items = get_items())
+    return render_index_response()
 
 if __name__ == '__main__':
     app.run()
