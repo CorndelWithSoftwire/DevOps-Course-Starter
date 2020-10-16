bae0d7607b9f0990c4571fe40dcace7e5e4cd66b
Mark McEwen <mark.mcewen@capita.com>
2020-10-16 12:37:00 +0100

    New method for filter TDD: ToDo

--- tests/test_view_model.py
+++ tests/test_view_model.py
@@ -1,19 +1,32 @@
+import pytest
 from todo_app.data.item import Item
 from todo_app.viewmodel import ViewModel
 
-
-def test_view_model_can_show_todo_items():
-  # Setup
+@pytest.fixture
+def generateItems():
   item1 = Item('1', "To Do", "A new todo", 'xx')
   item2 = Item('2', "Done", "A done todo", 'yy')
-  items = [
-    item1,
-    item2
-  ]
+  items = [ item1, item2 ]
+  return items
+    
+def test_view_model_can_show_all_items(generateItems):
+  # Setup via fixture
+
   # Act
-  view_model = ViewModel(items)
+  view_model = ViewModel(generateItems)
   todo_items = view_model.todo_items 
   
   # Assert
   assert len(todo_items) == 2
+
+
+def test_view_model_can_show_todo_items(generateItems):
+  # Setup via fixture
+
+  # Act
+  view_model = ViewModel(generateItems)
+  todo_items = view_model.todo_items 
+  
+  # Assert
+  assert len(todo_items) == 1
   assert todo_items[0].status == "To Do"
