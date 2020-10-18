fbac528cb4355e612c2aa96236877ad6a60df742
Vaibhav Sharma <vaibhav.gtalk@gmail.com>
2020-10-18 11:22:02 +0100

    Final commit Module 1 Exercise

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request,session
 
 from todo_app.flask_config import Config
+
+import session_items
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def index():
-    return 'Hello World!'
+    if request.method == 'POST':
+        title = request.form['title_of_todo']
+        if (title != ""):
+            session_items.add_item(title)
+        #return 'Hello World!'
+    todos = session_items.get_items()
+    return render_template('index.html',todos=todos)
 
 
 if __name__ == '__main__':
