8528779416afc4dc8c0c8df3d2168e95076ca687
kevineaton-devops <69510597+kevineaton-devops@users.noreply.github.com>
2020-10-19 15:04:55 +0100

    added more test lists

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,32 +1,52 @@
 import app, Trello_items, pytest
 
 TEST_LIST1 = [
-    Trello_items.Item('1','list saved todo items','doing'), 
-    Trello_items.Item('2','Allow new items to be added','Complete'),
-    Trello_items.Item('3','to do item','to do'),
-    Trello_items.Item('4','to do item','to do'),
-    Trello_items.Item('5','to do item','Complete'),
-    Trello_items.Item('6','to do item','to do'),
-    Trello_items.Item('7','to do item','Doing')
+    Trello_items.Item('1','ITEM1','Doing'), 
+    Trello_items.Item('2','ITEM2','Completed'),
+    Trello_items.Item('3','ITEM3','To Do'),
+    Trello_items.Item('4','ITEM4','To Do'),
+    Trello_items.Item('5','ITEM5','Completed'),
+    Trello_items.Item('6','ITEM6','To Do'),
+    Trello_items.Item('7','ITEM7','Doing')
     ]
 TEST_LIST2 = [
-    Trello_items.Item('1','list saved todo items','doing'), 
-    Trello_items.Item('2','Allow new items to be added','Complete'),
-    Trello_items.Item('3','to do item','to do'),
-    Trello_items.Item('4','to do item','to do'),
-    Trello_items.Item('5','to do item','Complete'),
-    Trello_items.Item('6','to do item','to do'),
-    Trello_items.Item('7','to do item','to do')
+    Trello_items.Item('1','ITEM1','To Do'), 
+    Trello_items.Item('2','ITEM2','To Do'),
+    Trello_items.Item('3','ITEM3','To Do'),
+    Trello_items.Item('4','ITEM4','To Do'),
+    Trello_items.Item('5','ITEM5','To Do'),
+    Trello_items.Item('6','ITEM6','To Do'),
+    Trello_items.Item('7','ITEM7','To Do')
     ]
 TEST_LIST3 = [
-    Trello_items.Item('1','list saved todo items','doing'), 
-    Trello_items.Item('2','Allow new items to be added','Complete'),
-    Trello_items.Item('5','to do item','Complete'),
+    Trello_items.Item('1','ITEM1','Doing'), 
+    Trello_items.Item('2','ITEM2','Doing'),
+    Trello_items.Item('3','ITEM3','Doing'),
+    Trello_items.Item('4','ITEM4','Doing'),
+    Trello_items.Item('5','ITEM5','Doing'),
+    Trello_items.Item('6','ITEM6','Doing'),
+    Trello_items.Item('7','ITEM7','Doing')
     ]
-TEST_LISTS = [TEST_LIST1, TEST_LIST2, TEST_LIST3]
+TEST_LIST4 = [
+    Trello_items.Item('1','ITEM1','Completed'), 
+    Trello_items.Item('2','ITEM2','Completed'),
+    Trello_items.Item('3','ITEM3','Completed'),
+    Trello_items.Item('4','ITEM4','Completed'),
+    Trello_items.Item('5','ITEM5','Completed'),
+    Trello_items.Item('6','ITEM6','Completed'),
+    Trello_items.Item('7','ITEM7','Completed')
+    ]
+TEST_LIST5 = [
+    Trello_items.Item('1','ITEM1','Doing'), 
+    Trello_items.Item('7','ITEM7','Completed'),
+    Trello_items.Item('5','ITEM5','Completed'),
+    ]
+TEST_LIST6 = [
+    ]
+TEST_LISTS = [TEST_LIST1, TEST_LIST2, TEST_LIST3, TEST_LIST4, TEST_LIST5, TEST_LIST6]
 
 @pytest.mark.parametrize("TEST_ITEMS", TEST_LISTS)
 def test_view_todoitems_contains_only_todo_items(TEST_ITEMS):
     view = app.ViewModel(TEST_ITEMS)
     for item in view.todoitems:
-        assert item.status == "to do"
+        assert item.status == "To Do"
