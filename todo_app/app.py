70fdb75c39f4aee7e16c4e827983d9aff01a526e
Alex Jones <alexander.jones@softwire.com>
2020-10-20 11:20:31 +0100

    Split out GET & POST endpoints

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template, request,session
+from flask import Flask, render_template, request, redirect
 
 from todo_app.flask_config import Config
 
-import session_items
+import todo_app.data.session_items as session_items
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-@app.route('/', methods = ['GET','POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        title = request.form['title_of_todo']
-        if (title != ""):
-            session_items.add_item(title)
-        #return 'Hello World!'
     todos = session_items.get_items()
     return render_template('index.html',todos=todos)
 
+@app.route('/add-item', methods = ["POST"])
+def add_item():
+    title = request.form['title_of_todo']
+    if (title != ""):
+        session_items.add_item(title)
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run()
