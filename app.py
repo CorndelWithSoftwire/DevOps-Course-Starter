0382ee71aa9a90fd62d9659ab85295e5427c403a
kevineaton-devops <69510597+kevineaton-devops@users.noreply.github.com>
2020-10-21 13:01:51 +0100

    moved app code into create_app function to prepare for integration testing

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 import Trello_items as trello
 import requests, datetime
 
-app = Flask(__name__)
-app.config.from_object('flask_config.Config')
-
 class ViewModel:
     def __init__(self, items):
         self._items = items
@@ -36,34 +33,38 @@ class ViewModel:
         older_done_items = [item for item in all_done_items if item.lastmodifieddate < today]    
         return older_done_items
 
-@app.route('/')
-def index():
-    items = trello.get_items_trello()
-    items=sorted(items, key=lambda k: k.status, reverse=True)
-    item_view_model = ViewModel(items)
-    return render_template('index.html', view_model=item_view_model)
-    #return render_template('index.html', items=items)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('flask_config.Config')
 
-@app.route('/<id>/completed', methods=['POST'])
-def completeditem(id):
-    trello.mark_item_done_trello(id)
-    return redirect('/') 
+    @app.route('/')
+    def index():
+        items = trello.get_items_trello()
+        items=sorted(items, key=lambda k: k.status, reverse=True)
+        item_view_model = ViewModel(items)
+        return render_template('index.html', view_model=item_view_model)
 
-@app.route('/<id>/todo', methods=['POST'])
-def todoitem(id):
-    trello.mark_item_todo_trello(id)
-    return redirect('/') 
+    @app.route('/<id>/completed', methods=['POST'])
+    def completeditem(id):
+        trello.mark_item_done_trello(id)
+        return redirect('/') 
 
-@app.route('/<id>/doing', methods=['POST'])
-def doingitem(id):
-    trello.mark_item_doing_trello(id)
-    return redirect('/') 
+    @app.route('/<id>/todo', methods=['POST'])
+    def todoitem(id):
+        trello.mark_item_todo_trello(id)
+        return redirect('/') 
 
-@app.route('/newitems', methods=['POST'])
-def newitems():
-    itemname = request.form.get('Title')
-    trello.add_item_trello(itemname)
-    return redirect('/') 
+    @app.route('/<id>/doing', methods=['POST'])
+    def doingitem(id):
+        trello.mark_item_doing_trello(id)
+        return redirect('/') 
 
-if __name__ == '__main__':
-    app.run()
+    @app.route('/newitems', methods=['POST'])
+    def newitems():
+        itemname = request.form.get('Title')
+        trello.add_item_trello(itemname)
+        return redirect('/') 
+
+    if __name__ == '__main__':
+        app.run()
+    return app
