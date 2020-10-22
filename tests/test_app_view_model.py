354016778230585f5ecd4ffd1f3bbe2f590beec9
Mark McEwen <mark.mcewen@capita.com>
2020-10-22 07:50:05 +0100

    Added more assertions

--- tests/test_app_view_model.py
+++ tests/test_app_view_model.py
@@ -6,5 +6,6 @@ def test_get_root(client, monkeypatch):
     response = client.get('/')
     assert response.status_code == 200
     assert str(response.data).find('/update_todo/5f6a132be45bd103bdaa16a9') > 0
+    assert str(response.data).find('test sunday') > 0
 
 
