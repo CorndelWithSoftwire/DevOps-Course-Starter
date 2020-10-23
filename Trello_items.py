4d2023316f0fe76daa441faf757566b6e1d0e050
kevineaton-devops <69510597+kevineaton-devops@users.noreply.github.com>
2020-10-23 17:24:06 +0100

    Refactored Trello_items to load environment variables and call the variables as functions when required

--- Trello_items.py
+++ Trello_items.py
@@ -1,16 +1,30 @@
 import requests
 import os, datetime
+from dotenv import load_dotenv
+load_dotenv()
 
-TRELLO_KEY = os.environ.get('TRELLO_KEY')
-TRELLO_TOKEN = os.environ.get('TRELLO_TOKEN')
-TRELLO_TODO_BOARDID = os.environ.get('TRELLO_TODO_BOARDID')
-TRELLO_TODO_LISTID = os.environ.get('TRELLO_TODO_LISTID')
-TRELLO_DOING_LISTID = os.environ.get('TRELLO_DOING_LISTID')
-TRELLO_DONE_LISTID = os.environ.get('TRELLO_DONE_LISTID')
+def get_trello_key():
+    TRELLO_KEY = os.environ.get('TRELLO_KEY')
+    return TRELLO_KEY
+def get_trello_token():
+    TRELLO_TOKEN = os.environ.get('TRELLO_TOKEN')
+    return TRELLO_TOKEN
+def get_trello_todo_boardid():
+    TRELLO_TODO_BOARDID = os.environ.get('TRELLO_TODO_BOARDID')
+    return TRELLO_TODO_BOARDID
+def get_trello_todo_listid():
+    TRELLO_TODO_LISTID = os.environ.get('TRELLO_TODO_LISTID')
+    return TRELLO_TODO_LISTID
+def get_trello_doing_listid():
+    TRELLO_DOING_LISTID = os.environ.get('TRELLO_DOING_LISTID')
+    return TRELLO_DOING_LISTID
+def get_trello_done_listid():
+    TRELLO_DONE_LISTID = os.environ.get('TRELLO_DONE_LISTID')
+    return TRELLO_DONE_LISTID
 
 apiurl = "https://api.trello.com/1/"
 boardsurl = apiurl + 'boards/'
-getcardsonboardsurl = boardsurl + TRELLO_TODO_BOARDID + '/cards'
+getcardsonboardsurl = boardsurl + get_trello_todo_boardid() + '/cards'
 cardsurl = apiurl + 'cards/'
 
 def getlistofcard_URL(cardid):
@@ -21,7 +35,7 @@ def putcardsonlist_URL(cardid):
     return URL
 
 def build_auth_query():
-    return {'key' : TRELLO_KEY, 'token' : TRELLO_TOKEN}
+    return {'key' : get_trello_key(), 'token' : get_trello_token()}
 
 class Item:
     def __init__(self, id, title, lastmodifieddate, status='To Do'):
@@ -47,19 +61,15 @@ def get_items_trello():
 
     Returns:
         list: The list of saved items.
-    """
-    TRELLO_TODO_LISTID = os.environ.get('TRELLO_TODO_LISTID')
-    TRELLO_DOING_LISTID = os.environ.get('TRELLO_DOING_LISTID')
-    TRELLO_DONE_LISTID = os.environ.get('TRELLO_DONE_LISTID')
-    
+    """   
     cards_json = get_items_from_trello_api()
     items = []
     for card in cards_json:
-        if card['idList'] == TRELLO_DONE_LISTID:
+        if card['idList'] == get_trello_done_listid():
             cardstatus = 'Done'
-        elif card['idList'] == TRELLO_DOING_LISTID:
+        elif card['idList'] == get_trello_doing_listid():
             cardstatus = 'Doing'
-        elif card['idList'] == TRELLO_TODO_LISTID:
+        elif card['idList'] == get_trello_todo_listid():
             cardstatus = "To Do"
         else:
             raise AttributeError
@@ -77,7 +87,7 @@ def mark_item_done_trello(id):
         item: The ID of the item to save.
     """
     query = build_auth_query()
-    query['idList'] = TRELLO_DONE_LISTID
+    query['idList'] = get_trello_done_listid()
     requests.put(putcardsonlist_URL(id), params=query)
     return id
 
@@ -89,7 +99,7 @@ def mark_item_todo_trello(id):
         item: The ID of the item to save.
     """
     query = build_auth_query()
-    query['idList'] = TRELLO_TODO_LISTID
+    query['idList'] = get_trello_todo_listid()
     requests.put(putcardsonlist_URL(id), params=query)
     return id
 
@@ -101,7 +111,7 @@ def mark_item_doing_trello(id):
         item: The ID of the item to save.
     """
     query = build_auth_query()
-    query['idList'] = TRELLO_DOING_LISTID
+    query['idList'] = get_trello_doing_listid()
     requests.put(putcardsonlist_URL(id), params=query)
     return id
 
@@ -116,7 +126,7 @@ def add_item_trello(title):
         item: The saved item.
     """
     query = build_auth_query()
-    query['idList'] = TRELLO_TODO_LISTID
+    query['idList'] = get_trello_todo_listid()
     query['name'] = title
     requests.post(cardsurl, params=query)
     return 
