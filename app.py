d22bc5a71c6732b40c558ee1e93f17f56371e1a8
BerserkerLan <rusab3529>
2020-10-25 21:18:02 +0000

    Completed fully now

--- app.py
+++ app.py
@@ -37,16 +37,22 @@ def submitNewItem():
         item_list.append(items['name'])
     return render_template("index.html", items=item_list)
 
-@app.route('/markAsComplete/<itemName>', methods=['POST'])
-def markItemAsComplete(itemName):
-    print(itemName)
+@app.route('/markAsComplete/<item_id>')
+def markAsComplete(item_id):
+    print(item_id)
     item_list = []
     list_id = '5f56323626c33d81cd98b386'
     r = Requests.get('https://api.trello.com/1/lists/{}/cards?key={}&token={}'.format(list_id, secrets.KEY, secrets.TOKEN))
-    for items in (r.json()):
-        if (items['name'] == itemName):
-            print(items)
-        item_list.append(items['name'])
+    items_list = r.json()
+    item_key = ''
+    for item in items_list:
+        if (item['name'] == item_id):
+            item_key = item['id']
+    print("Item Key: {}", item_key)
+    list_to_move_to = '5f56324465484e35d83eb45b'
+    url = Requests.put('https://api.trello.com/1/cards/{}?key={}&token={}&idList={}'.format(item_key, secrets.KEY, secrets.TOKEN, list_to_move_to))
+    print("Json")
+    print(url.text)
     return render_template("index.html", items=item_list)    
 
 if __name__ == '__main__':
