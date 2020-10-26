5a30fca642f55a81795d894e8b5c44aa4a2cafa9
kevineaton-devops <69510597+kevineaton-devops@users.noreply.github.com>
2020-10-26 15:21:43 +0000

    introduced create_trello_board and delete_trello_board

---
+++ Trello_boards.py
@@ -0,0 +1,35 @@
+import requests
+from dotenv import load_dotenv
+from Trello_items import boardsurl, get_trello_key, get_trello_token, build_auth_query
+load_dotenv()
+
+def create_trello_board():
+    """
+    Adds a new board in Trello with the given name
+
+    Args:
+        title: The name of the Board.
+
+    Returns:
+        item: The saved item.
+    """
+    query = build_auth_query()
+    query['name'] = "Test_Board"
+    response = requests.post(boardsurl, params=query)
+    response_json = response.json()
+    return response_json['id']
+
+def delete_trello_board(id):
+    """
+    deletes the board in Trello with the given id
+
+    Args:
+        id: The id of the Board.
+
+    Returns:
+        : 
+    """
+    idboardurl = boardsurl + id
+    requests.delete(idboardurl, params=build_auth_query())
+    return 
+    
