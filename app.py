e4b8bd652d0e71013e4e6987d760120a85ff2d51
Shajeeth Sushama <shajeethsushama@Shajeeths-MacBook-Pro.local>
2020-11-17 16:48:05 +0000

    First Exercise-Shajeeth

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for
+
+from flask import Flask, render_template, redirect, url_for, request
+
 import session_items as session
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = session.get_items()
+    return render_template('index.html', items = items)
+
+
+@app.route('/items/new', methods=['POST'])
+def add_item():
+    title = request.form['title']
+    session.add_item(title)
+    return redirect(url_for('index'))
+
+
+@app.route('/items/<id>/complete')
+def complete_item(id):
+    session.save_item(id)
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
