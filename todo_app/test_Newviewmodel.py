3f5f5ef629a12e594677ebbeb84de6b9d84ddc6a
Dave Rawlinson <drawlinson48@gmail.com>
2021-02-09 15:09:02 +0000

    sdfghshfd

---
+++ todo_app/test_Newviewmodel.py
@@ -0,0 +1,17 @@
+import pytest
+from todo_app.todo import Newviewmodel    
+
+
+def test_from_trello_card():
+	# Given 
+	test_array=[1,2,3,4]
+	test_Newviewmodel=Newviewmodel(test_array)
+	# When
+	result=test_Newviewmodel.items
+	# Then
+	assert result == test_array
+
+
+
+
+
