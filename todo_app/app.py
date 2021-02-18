d5eaeea81ba384698080165f09b4946555e87f58
charles knighton <charles.knighton@capita.com>
2021-02-18 10:08:57 +0000

    Commiting latest for module1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from todo_app.data import session_items
 
 from todo_app.flask_config import Config
@@ -14,13 +14,21 @@ def index():
     return render_template('index.html', items = todo_items)
 
 @app.route('/add', methods=['GET', 'POST'])
-def add_item(id,title,status):
+def add_item():
     #add_todo_item = session_items.add_item(id, title, status)
-    id = request.forms.get('id')
-    title = request.forms.get('title')
-    status = request.forms.get('status')
+    id = request.form.get('id')
+    title = request.form.get('title')
+    status = request.form.get('status')
     #print = "Item added " + id + " " + title + " " + status + "."
-    return render_template('index.html')
+    #return render_template('index.html')
+    todo_items = session_items.add_item(title) #finally display the new item
+    return redirect('/')
+
+@app.route('/item/{{ item.id }}')
+def get_items(id,title,status):
+    id =  get_items('id')
+    return render_template('index.html', items = todo_items)
+    
 
 if __name__ == '__main__':
     app.run()
