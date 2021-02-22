6523439bb601cc3eedd6eb1fd7cf3c36d86599ae
Arpitha <arpitha.mankhala@capita.co.uk>
2021-02-22 14:38:27 +0000

    Exercise 1 for Module1 Commit

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from todo_app.data import session_items
 
 from todo_app.flask_config import Config
 
@@ -8,7 +9,22 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = session_items.get_items()
+    return render_template('index.html', items=items)
+
+@app.route('/additem', methods =["POST"])
+def add_item():
+    session_items.add_item(request.form.get("title"))
+    return redirect(url_for("index"))
+    
+
+@app.route('/<id>')
+def get_item(id):
+    item = session_items.get_item(id)
+    return f"Item returned is {item['title']}"
+
+
+
 
 
 if __name__ == '__main__':
