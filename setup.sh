f8d19c80bf0e65bafddbb4f5cc609e12827c4e1c
Chloe McDermott <chloe.mcdermott@capita.co.uk>
2021-03-22 12:02:08 +0000

    amendments to set upsscript

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
+#Checking if poetry is already installed
+poetry --version 
+
 # Installing poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 
 # Add poetry to PATH in script
 export POETRY_ROOT=\"$HOME//.poetry/\"
 export PATH=\"$POETRY_ROOT/bin:$PATH\"
-
-#Checking if poetry is already installed
-poetry --version 
 
 #Install Python
 sudo apt-get update -y && sudo apt-get install -y python3.8 
