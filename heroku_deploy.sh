cbea7f6a637229f676c7f96a6aef49ebb29c2cc4
okpokpoaniekan@gmail.com <aniekan.okpokpo@aexp.com>
2021-03-24 02:26:00 +0000

    working build and deploy to docker and heroku

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "Deploying to Heroku"
-docker tag todo-app:prod registry.heroku.com/todo-test-application/web
-docker push registry.heroku.com/todo-test-application/web
-heroku container:release web -a todo-test-application
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  echo "Deploying app to Heroku";
+  echo "Tagging heroku docker image";
+  docker tag $DOCKER_USERNAME/todo-app registry.heroku.com/todo-test-application/web;
+  echo "LOGGING IN TO HEROKU";
+  echo "$HEROKU_API_KEY" | docker login -u "$HEROKU_LOGIN" --password-stdin registry.heroku.com;
+  echo "Pushing to Heroku docker registry";
+  docker push registry.heroku.com/todo-test-application/web;
+  echo "releasing application";
+  heroku container:release web -a todo-test-application;
+fi
