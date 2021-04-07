f7a140d1da69d4e41851e333659f14a085c5e7db
kevineaton-devops <69510597+kevineaton-devops@users.noreply.github.com>
2021-04-07 20:01:38 +0100

    corrected docker name for cache

--- todo_app/deploy.sh
+++ todo_app/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
-docker build --target production --cache-from kevineatondevops/todo_app:latest --tag kevineatondevops/todo-app:latest --tag kevineatondevops/todo-app:$TRAVIS_COMMIT --tag registry.heroku.com/kedevopstodoapp/web .
+docker build --target production --cache-from kevineatondevops/todo-app:latest --tag kevineatondevops/todo-app:latest --tag kevineatondevops/todo-app:$TRAVIS_COMMIT --tag registry.heroku.com/kedevopstodoapp/web .
 docker push kevineatondevops/todo-app:latest
 docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
 docker push registry.heroku.com/kedevopstodoapp/web
