c1903282e11c2b723d880ef7b976793f4b63a228
kevineaton-devops <69510597+kevineaton-devops@users.noreply.github.com>
2021-04-07 16:31:45 +0100

    updated heroku login

--- todo_app/deploy.sh
+++ todo_app/deploy.sh
@@ -2,6 +2,6 @@
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
 docker build --target production --tag kevineatondevops/todo-app:latest --tag kevineatondevops/todo-app:$TRAVIS_COMMIT --tag registry.heroku.com/kedevopstodoapp/web .
 docker push kevineatondevops/todo-app:latest
-docker login --username=kevin.eaton@capita.com --password=$(heroku auth:token) registry.heroku.com
+docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
 docker push registry.heroku.com/kedevopstodoapp/web
 heroku container:release web -a kedevopstodoapp
