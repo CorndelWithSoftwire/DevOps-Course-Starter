f6834d8845765c89bf70b275fa9db417e447ce35
Raj Rahman <raj.rahman@outlook.com>
2021-04-08 17:41:25 +0100

    added flask login to poetry

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('flask_config.Config')
 
-    login_manager = LoginManager()
+    login_managers = LoginManager()
 
     client_id = os.getenv("GIT_CLIENT_ID")
     client_secret = os.getenv("GIT_CLIENT_SECRET")
     authorization_base_url = 'https://github.com/login/oauth/authorize'
 
-    @login_manager.unauthorized_handler
+    @login_managers.unauthorized_handler
     def unauthenticated():
         client = WebApplicationClient(client_id)       
         uri = client.prepare_request_uri(authorization_base_url)
@@ -23,19 +23,20 @@ def create_app():
         return redirect(uri)
         #pass
 
-    @login_manager.user_loader
+    @login_managers.user_loader
     def load_user(user_id):
         return None
     
 
-    @login_required
+    
     @app.route('/')
+    @login_required
     def index():
         mongo_items.get_items()
         return redirect('tasks')
     
-    @login_required
     @app.route('/tasks')
+    @login_required
     def list_tasks():
         items = mongo_items.get_items()
         # Get ToDo items
@@ -43,38 +44,42 @@ def create_app():
 
         return render_template('index.html', view_model=view_model)
 
-    @login_required
     @app.route('/add', methods=['POST'])
+    @login_required
     def add_item():
         task = request.form['task_description']
         mongo_items.add_item(task)
 
         return redirect(url_for('index'))
 
-    @login_required
     @app.route('/tasks/<id>/complete', methods=['POST'])
+    @login_required
     def complete_item(id):
         mongo_items.markAsDone(id)
 
         return redirect(url_for('index'))
 
-    @login_required
     @app.route('/tasks/<id>/remove', methods=['POST'])
+    @login_required
     def remove_item(id):
         mongo_items.remove_item(id)
 
         return redirect(url_for('index'))
 
-    @login_required
     @app.route('/tasks/<id>/inprogress', methods=['POST'])
+    @login_required
     def inprogress_item(id):
         mongo_items.inprogress_item(id)
 
         return redirect(url_for('index'))
 
-    # All the routes and setup code etc
+    @app.route('/callback')
+    def callback():
+        ###
+        return redirect('tasks')
 
-    login_manager.init_app(app)
+
+    login_managers.init_app(app)
 
     return app
 
