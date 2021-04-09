c66552a4d192bc179c8876ea2cb4d06d3eced700
ncaunhye <nadia.caunhye@kpmg.co.uk>
2021-04-09 13:49:50 +0100

    module-3 current progress
    Delete trello.py

--- app.py
+++ app.py
@@ -1,12 +1,153 @@
 from flask import Flask, render_template, request, redirect, url_for
-import session_items as session
+import session_items
+import pdb
+from datetime import datetime, timedelta
 
-app = Flask(__name__)
-app.config.from_object('flask_config.Config')
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+class ViewModel:
+    def __init__(self, items):
+        self._items = items
 
-if __name__ == '__main__':
-    app.run()
+    @property
+    def items(self):
+        return self._items
+
+    @property
+    def todo_items(self):
+        todo_items = []
+        for item in self._items:
+            if item["status"] == "To Do":
+                todo_items.append(item)
+            else:
+                continue
+        return todo_items
+
+    @property
+    def in_progress_items(self):
+        in_progress_items = []
+        for item in self._items:
+            if item["status"] == "In Progress":
+                in_progress_items.append(item)
+            else:
+                continue
+        return in_progress_items
+
+    @property
+    def complete_items(self):
+        complete_items = []
+        for item in self._items:
+            if item["status"] == "Complete":
+                complete_items.append(item)
+            else:
+                continue
+        return complete_items
+
+    @property
+    def show_all_done_items(self):
+        if len(self.complete_items) <= 5:
+            return True
+        else:
+            return False
+
+    @property
+    def recent_done_items(self):
+        datetime_yesterday = datetime.now() - timedelta(minutes=1)
+        recent_done_items = []
+        for item in self._items:
+            if (item["status"] == "Complete") and (
+                item["last_edited"] > datetime_yesterday
+            ):
+                recent_done_items.append(item)
+            else:
+                continue
+
+        return recent_done_items
+
+    @property
+    def older_done_items(self):
+        datetime_yesterday = datetime.now() - timedelta(minutes=1)
+        older_done_items = []
+        for item in self._items:
+            if (item["status"] == "Complete") and (
+                item["last_edited"] < datetime_yesterday
+            ):
+                older_done_items.append(item)
+            else:
+                continue
+
+        return older_done_items
+
+
+def create_app():
+    app = Flask(__name__) 
+    app.config.from_object('flask_config.Config')
+
+    @app.route("/")
+    def index():
+        items = session_items.get_items()
+        item_view_model = ViewModel(items)
+
+
+        return render_template("index.html", view_model=item_view_model)
+
+
+    @app.route("/create_item", methods=["POST"])
+    def create_item():
+        new_dict = request.form.to_dict(flat=False)
+        for item_name in new_dict["newitem"]:
+            session_items.add_item(item_name)
+
+        return redirect("/")
+
+
+    # @app.route("/mark_in_progress", methods=["POST"])
+    # def mark_in_progress():
+    #     new_dict = request.form.to_dict(flat=False)
+    #     for id in new_dict.keys():
+    #         item = session_items.get_item(id)
+    #         session_items.mark_in_progress(item)
+
+    #     return redirect("/")
+
+
+    # @app.route("/mark_as_complete", methods=["POST"])
+    # def mark_as_complete():
+    #     new_dict = request.form.to_dict(flat=False)
+    #     for id in new_dict.keys():
+    #         item = session_items.get_item(id)
+    #         session_items.mark_complete(item)
+
+    #     return redirect("/")
+
+
+    @app.route("/update_status", methods=["POST"])
+    def update_status():
+        new_dict = request.form.to_dict(flat=False)
+        string_select_update = new_dict["select_update"][0]
+        if "item_id" in new_dict:
+            item = session_items.get_item(int(new_dict["item_id"][0]))
+            session_items.update_status(item, string_select_update)
+
+        return redirect("/")
+
+
+    @app.route("/delete_item", methods=["POST"])
+    def delete_item():
+        new_dict = request.form.to_dict(flat=False)
+        for id in new_dict.keys():
+            item = session_items.get_item(id)
+            session_items.delete_item(item)
+
+        return redirect("/")
+
+    @app.route("/due_date", methods=["POST"])
+    def due_date(): 
+        new_dict = request.form.to_dict(flat=False)
+        due_date_update = new_dict["duedate"][0]
+        if "item_id" in new_dict:
+            item = session_items.get_item(int(new_dict["item_id"][0]))
+            session_items.due_date(item, due_date_update)
+        
+        return redirect("/")
+
+    return app
--- session_items.py
+++ session_items.py
@@ -1,9 +1,21 @@
 from flask import session
+import requests
+import pdb
+import os
+import pprint
+from datetime import datetime
+from dateutil.parser import parse
 
 _DEFAULT_ITEMS = [
-    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
-    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+    {"id": 1, "status": "Not Started", "title": "List saved todo items"},
+    {"id": 2, "status": "Not Started", "title": "Allow new items to be added"},
 ]
+query = {
+    "key": os.getenv("Key"),
+    "token": os.getenv("Token"),
+}
+
+get_cards_url = f"https://api.trello.com/1/boards/{os.getenv('Board_ID')}/cards"
 
 
 def get_items():
@@ -13,7 +25,47 @@ def get_items():
     Returns:
         list: The list of saved items.
     """
-    return session.get('items', _DEFAULT_ITEMS)
+
+    cards = requests.get(get_cards_url, params=query).json()
+    items = []
+    for card in cards:
+        if card["due"] == None:
+            due = ""
+        else:
+            due = parse(card["due"]).strftime('%d-%m-%Y')
+
+        if card["idList"] == os.getenv("to_do_list"):
+            items.append(
+                {
+                    "id": card["idShort"],
+                    "status": "To Do",
+                    "title": card["name"],
+                    "last_edited": parse(card["dateLastActivity"]).replace(tzinfo=None),
+                    "due_date" : due
+                }
+            )
+        elif card["idList"] == os.getenv("in_progress_list"):
+            items.append(
+                {
+                    "id": card["idShort"],
+                    "status": "In Progress",
+                    "title": card["name"],
+                    "last_edited": parse(card["dateLastActivity"]).replace(tzinfo=None),
+                    "due_date" : due
+                }
+            )
+        elif card["idList"] == os.getenv("complete_list"):
+            items.append(
+                {
+                    "id": card["idShort"],
+                    "status": "Complete",
+                    "title": card["name"],
+                    "last_edited": parse(card["dateLastActivity"]).replace(tzinfo=None),
+                    "due_date" : due
+                }
+            )
+
+    return items
 
 
 def get_item(id):
@@ -27,7 +79,11 @@ def get_item(id):
         item: The saved item, or None if no items match the specified ID.
     """
     items = get_items()
-    return next((item for item in items if item['id'] == int(id)), None)
+
+    return next((item for item in items if item["id"] == int(id)), None)
+    for item in items:
+        if item["id"] == int(id):
+            return item
 
 
 def add_item(title):
@@ -42,16 +98,9 @@ def add_item(title):
     """
     items = get_items()
 
-    # Determine the ID for the item based on that of the previously added item
-    id = items[-1]['id'] + 1 if items else 0
-
-    item = { 'id': id, 'title': title, 'status': 'Not Started' }
-
-    # Add the item to the list
-    items.append(item)
-    session['items'] = items
-
-    return item
+    url = f"https://api.trello.com/1/cards"
+    data = {"idList": os.getenv("to_do_list"), "name": title}
+    response = requests.request("POST", url, data=data, params=query)
 
 
 def save_item(item):
@@ -62,8 +111,74 @@ def save_item(item):
         item: The item to save.
     """
     existing_items = get_items()
-    updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
-
-    session['items'] = updated_items
-
+    # updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
+    updated_items = []
+    for existing_item in existing_items:
+        if item["id"] == existing_item["id"]:
+            updated_items.append(item)
+        else:
+            updated_items.append(existing_item)
+    session["items"] = updated_items
     return item
+
+
+def delete_item(item):
+    cards = requests.get(get_cards_url, params=query).json()
+    for card in cards:
+        print(card["id"], item["id"])
+        if card["id"] == item["id"]:
+            id = card["id"]
+            url = f"https://api.trello.com/1/cards/{id}"
+            response = requests.delete(url, params=query)
+
+# def mark_in_progress(item):
+#     cards = requests.get(get_cards_url, params=query).json()
+#     for card in cards:
+#         if card["name"] == item["title"]:
+#             id = card["id"]
+#             url = f"https://api.trello.com/1/cards/{id}"
+#             data = {"idList": os.getenv("in_progress_list")}
+#             response = requests.put(url, data=data, params=query)
+
+
+# def mark_complete(item):
+#     cards = requests.get(get_cards_url, params=query).json()
+#     for card in cards:
+#         if card["name"] == item["title"]:
+#             id = card["id"]
+#             url = f"https://api.trello.com/1/cards/{id}"
+#             data = {"idList": os.getenv("complete_list")}
+#             response = requests.put(url, data=data, params=query)
+
+def update_status(item, string_select_update): 
+    cards = requests.get(get_cards_url, params=query).json()
+    # pdb.set_trace()
+    for card in cards:
+        if string_select_update == "In-Progress":
+            if card["name"] == item["title"]:
+                id = card["id"]
+                url = f"https://api.trello.com/1/cards/{id}"
+                data = {"idList": os.getenv("in_progress_list")}
+                response = requests.put(url, data=data, params=query)
+        if string_select_update == "To-Do":
+            if card["name"] == item["title"]:
+                id = card["id"]
+                url = f"https://api.trello.com/1/cards/{id}"
+                data = {"idList": os.getenv("to_do_list")}
+                response = requests.put(url, data=data, params=query)
+        if string_select_update == "Complete":
+            if card["name"] == item["title"]:
+                id = card["id"]
+                url = f"https://api.trello.com/1/cards/{id}"
+                data = {"idList": os.getenv("complete_list")}
+                response = requests.put(url, data=data, params=query)
+
+
+def due_date(item, due_date_update): 
+    cards = requests.get(get_cards_url, params=query).json()
+    for card in cards:
+            if card["name"] == item["title"]:
+                id = card["id"]
+                url = f"https://api.trello.com/1/cards/{id}"
+                data = {"due": due_date_update}
+                response = requests.put(url, data=data, params=query)
