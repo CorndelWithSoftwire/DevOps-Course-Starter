a71cd5814ee3df7cbbdc4a5c9b961e7fb1f81c45
Raj Rahman <raj.rahman@outlook.com>
2021-04-25 19:53:04 +0100

    complete mod 10

--- app.py
+++ app.py
@@ -1,31 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for, session, make_response
+from flask import Flask, render_template, request, redirect, url_for, session, make_response, flash
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
-#from flask_user import roles_required
 import mongo_items as mongo_items
 from requests_oauthlib import OAuth2Session
 import items_view_model as ItemsViewModel
 import os
 import requests
 from oauthlib.oauth2 import WebApplicationClient
-from flask.json import jsonify
 from user import User
-#import login_manager as login_manager
 
 def create_app():
     app = Flask(__name__)
+    Flask.make_null_session
     app.config.from_object('flask_config.Config')
-    #app.secret_key = os.getenv("SECRET_KEY")
-    #app.config["SECRET_KEY"]=os.getenv("SECRET_KEY")
+    app.config['LOGIN_DISABLED'] = os.environ.get('LOGIN_DISABLED', 'False').lower() in ('true', 't', '1')
 
     login_managers = LoginManager()
-    login_managers.init_app(app)
 
     client_id = os.getenv("GIT_CLIENT_ID")
     client_secret = os.getenv("GIT_CLIENT_SECRET")
     authorization_base_url = 'https://github.com/login/oauth/authorize'
-
-    #login_manager.login_managers.init_app(app)
-
 
     @login_managers.unauthorized_handler
     def unauthenticated():
@@ -36,60 +29,64 @@ def create_app():
 
     @login_managers.user_loader
     def load_user(user_id):
-        try:
-            return User(user_id)
-        except:
-            return None
+        
+        return User(user_id)
+
 
     @app.route('/')
-    #@login_required
     def index():
-        if current_user.is_authenticated:
-            print("User logged in")
-        else:
-            print("User not logged in")
+        if current_user.is_authenticated is False:
+            flash('You must be logged in to manage tasks','alert alert-danger')
 
-        # items = mongo_items.get_items()
-        # view_model = ItemsViewModel.ItemsViewModel(items)
-
-        # return render_template('index.html', view_model=view_model)
-        return redirect('tasks')
-    
-    @app.route('/tasks')
-    @login_required
-    def list_tasks():
         items = mongo_items.get_items()
         # Get ToDo items
         view_model = ItemsViewModel.ItemsViewModel(items)
 
         return render_template('index.html', view_model=view_model)
+        
+    
+    @app.route('/tasks')
+    def list_tasks():
+
+        return redirect(url_for('index'))
 
     @app.route('/add', methods=['POST'])
     @login_required
     def add_item():
         task = request.form['task_description']
-        mongo_items.add_item(task)
-
+        if(current_user.userrole == 'writer'):
+            mongo_items.add_item(task)
+        else:
+            flash('You must be logged in to add tasks','alert alert-danger')
         return redirect(url_for('index'))
 
     @app.route('/tasks/<id>/complete', methods=['POST'])
     @login_required
     def complete_item(id):
-        mongo_items.markAsDone(id)
+        if(current_user.userrole == 'writer'):
+            mongo_items.markAsDone(id)
+        else:
+            flash('You must be logged in to Complete tasks','alert alert-danger')
 
         return redirect(url_for('index'))
 
     @app.route('/tasks/<id>/remove', methods=['POST'])
     @login_required
     def remove_item(id):
-        mongo_items.remove_item(id)
+        if(current_user.userrole == 'writer'):
+            mongo_items.remove_item(id)
+        else:
+            flash('You must be logged in to Remove tasks','alert alert-danger')
 
         return redirect(url_for('index'))
 
     @app.route('/tasks/<id>/inprogress', methods=['POST'])
     @login_required
     def inprogress_item(id):
-        mongo_items.inprogress_item(id)
+        if(current_user.userrole == 'writer'):
+            mongo_items.inprogress_item(id)
+        else:
+            flash('<strong>Success!</strong>You must be logged in to InProgress tasks','alert alert-danger')
 
         return redirect(url_for('index'))
 
@@ -102,7 +99,7 @@ def create_app():
         git_json = client.parse_request_body_response(git_access_key.text)
         git_user_request = client.add_token("https://api.github.com/user")
         git_user = requests.get(git_user_request[0], headers=git_user_request[1]).json()
-        git_login = User(git_user['id'])
+        git_login = User(git_user['login'])
         login_user(git_login)
 
         return redirect('/')
@@ -111,14 +108,14 @@ def create_app():
     @login_required
     def logout():
         logout_user()  
+        flash('You have been logged out','alert alert-success')
+        session.clear()
         return redirect('/')
 
 
+    login_managers.init_app(app)
 
+    if __name__ == '__main__':
+        app.run()
 
     return app
-
-
-if __name__ == '__main__':
-    create_app().run()
-
--- user.py
+++ user.py
@@ -1,9 +1,13 @@
-
 from flask_login import UserMixin, current_user, login_required, login_user, logout_user
-
 
 class User(UserMixin):
     def __init__(self, id): 
         self.id = id 
     
+    @property
+    def userrole(self):
+        if(self.id == 'rajrahman'):
+             return 'writer'
+        else:
+             return 'reader'
 
