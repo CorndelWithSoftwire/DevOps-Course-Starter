59dc24d89dc13c3dce1233191c41662dc62ea44c
kevineaton-devops <69510597+kevineaton-devops@users.noreply.github.com>
2021-04-30 09:12:23 +0100

    added docker push for travis_commit

--- todo_app/deploy.sh
+++ todo_app/deploy.sh
@@ -2,6 +2,7 @@
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
 docker build --target production --cache-from kevineatondevops/todo-app:latest --tag kevineatondevops/todo-app:latest --tag kevineatondevops/todo-app:$TRAVIS_COMMIT --tag registry.heroku.com/kedevopstodoapp/web .
 docker push kevineatondevops/todo-app:latest
-docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
+docker push kevineatondevops/todo-app:$TRAVIS_COMMIT
+echo $HEROKU_API_KEY | docker login --username=_ --password-stdin registry.heroku.com
 docker push registry.heroku.com/kedevopstodoapp/web
 heroku container:release web -a kedevopstodoapp
