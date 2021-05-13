7425c7a81b566b1df325a3910d6def86ef3946da
kevineaton-devops <69510597+kevineaton-devops@users.noreply.github.com>
2021-05-13 16:50:02 +0100

    added _item_todo functions

--- todo_app/Mongo_items.py
+++ todo_app/Mongo_items.py
@@ -79,12 +79,36 @@ def mark_doing_item_done_mongo(id):
     done.insert_one(doing.find_one({"_id" : ObjectId(id) })).inserted_id
     doing.delete_one(doing.find_one({"_id" : ObjectId(id) }))
 
+def mark_done_item_todo_mongo(id):
+    """
+    sets an existing done item in mongo to the todo collection and deletes from the done collection
 
+    Args:
+        item: The ID of the item to update.
+    """
+    todo = db.todo
+    done = db.done
+    todo.insert_one(done.find_one({"_id" : ObjectId(id) })).inserted_id
+    done.delete_one(done.find_one({"_id" : ObjectId(id) }))
+
+def mark_doing_item_todo_mongo(id):
+    """
+    sets an existing doing item in mongo to the todo collection and deletes from the doing collection
+
+    Args:
+        item: The ID of the item to update.
+    """
+    todo = db.todo
+    doing = db.doing
+    todo.insert_one(doing.find_one({"_id" : ObjectId(id) })).inserted_id
+    doing.delete_one(doing.find_one({"_id" : ObjectId(id) }))
 
 
 
 #add_item_mongo("todo_item_done")
 #mark_todo_item_doing_mongo("609d43d9f69eaec5f040b4c3")
-#mark_done_item_doing_mongo("60916afb46d633ac495437b3")
+#mark_done_item_doing_mongo("609d48b25a29536c2b6d32b0")
 #mark_todo_item_done_mongo("609d48b25a29536c2b6d32b0")
 #mark_doing_item_done_mongo("60916ac16b039134c61377f1")
+#mark_done_item_todo_mongo("60916ac16b039134c61377f1")
+#mark_doing_item_todo_mongo("609d48b25a29536c2b6d32b0")
