f43df6e47c63a5bd0451b832b23b5305313afb3e
kevineaton-devops <69510597+kevineaton-devops@users.noreply.github.com>
2021-05-20 10:26:18 +0100

    removed load_dotenv

--- todo_app/mongo_config.py
+++ todo_app/mongo_config.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import find_dotenv, load_dotenv
 
-file_path = find_dotenv('.env')
-load_dotenv(file_path, override=False)
+#file_path = find_dotenv('.env')
+#load_dotenv(file_path, override=False)
 
 class Config:
     MONGO_USER = os.environ.get("MONGO_USER")
