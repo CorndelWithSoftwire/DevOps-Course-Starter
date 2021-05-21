1af60f302eeef162d0aa32d1dfc90536e0d94503
Raj Rahman <raj.rahman@outlook.com>
2021-05-22 00:01:46 +0100

    remove dupe tests

--- test_mongo.py
+++ test_mongo.py
@@ -41,8 +41,8 @@ def test_app():
     os.environ['MONGO_LIST_DONE'] = 'done'
     os.environ['LOGIN_DISABLED'] = 'True'
     
-    mongo_conn = os.environ.get('MONGO_CONN')
-    mongo_conn = "mongodb+srv://rajrahman:NWyZIpz9kFnOW4dn@corndel.gyf3r.mongodb.net/test?w=majority"
+    mongo_conn = os.environ.get('MONGO_TEST_CONN')
+#    mongo_conn = "mongodb+srv://rajrahman:NWyZIpz9kFnOW4dn@corndel.gyf3r.mongodb.net/test?w=majority"
     os.environ['MONGO_CONN'] = mongo_conn
     client = pymongo.MongoClient(mongo_conn)
     db = client[os.environ['MONGO_DB_NAME']]
