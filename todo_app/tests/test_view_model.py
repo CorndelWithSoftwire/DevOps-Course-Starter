5beee9cfa057b58929d0603d4b4f47246624f4d5
ncaunhye <nadia.caunhye@kpmg.co.uk>
2021-06-30 15:29:45 +0100

    improving test functions

--- todo_app/tests/test_view_model.py
+++ todo_app/tests/test_view_model.py
@@ -10,7 +10,7 @@ def view_model():
     _DEFAULT_CARDS = [
         {
             "idShort": 1,
-            "idList": os.getenv("to_do_list"),
+            "idList": os.getenv("in_progress_list"),
             "name": "List saved todo items",
             "dateLastActivity": '2021-06-18T11:27:41.098Z',
             "due": '2030-06-18T11:27:41.098Z'
@@ -50,18 +50,26 @@ def test_items(view_model):
 
 
 def test_todo_items(view_model):
-    for item in view_model.todo_items:
-        assert item.status == "To Do"
-
+    if len(view_model.todo_items) >= 1:
+        for item in view_model.todo_items:
+            assert item.status == "To Do"
+    else: 
+        assert view_model.todo_items == []
 
 def test_in_progress_items(view_model):
-    for item in view_model.in_progress_items:
-        assert item.status == "In Progress"
+    if len(view_model.in_progress_items) >= 1:
+        for item in view_model.in_progress_items:
+            assert item.status == "In Progress"
+    else: 
+        assert view_model.in_progress_items == []
 
 
 def test_complete_items(view_model):
-    for item in view_model.complete_items:
-        assert item.status == "Complete"
+    if len(view_model.complete_items) >= 1:
+        for item in view_model.complete_items:
+            assert item.status == "Complete"
+    else: 
+        assert view_model.complete_items == []
 
 
 def test_show_all_done_items(view_model):
