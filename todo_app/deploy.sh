c49f3146e94e3c48fe8e33858c5847407694bb6a
kevineaton-devops <69510597+kevineaton-devops@users.noreply.github.com>
2021-07-19 14:09:03 +0100

    added webhook and removed heroku

--- todo_app/deploy.sh
+++ todo_app/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
-docker build --target production --cache-from kevineatondevops/todo-app:latest --tag kevineatondevops/todo-app:latest --tag kevineatondevops/todo-app:$TRAVIS_COMMIT --tag registry.heroku.com/kedevopstodoapp/web .
+docker build --target production --cache-from kevineatondevops/todo-app:latest --tag kevineatondevops/todo-app:latest --tag kevineatondevops/todo-app:$TRAVIS_COMMIT .
 docker push kevineatondevops/todo-app:latest
 docker push kevineatondevops/todo-app:$TRAVIS_COMMIT
-echo $HEROKU_API_KEY | docker login --username=_ --password-stdin registry.heroku.com
 docker push registry.heroku.com/kedevopstodoapp/web
-heroku container:release web -a kedevopstodoapp
+curl -dH -X POST $AZURE_WEBHOOK
