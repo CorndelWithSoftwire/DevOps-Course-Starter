1f578c7f7e167c13d1ebf32a71b19dbbe64d5a44
FrancisIris <93911682+FrancisIris@users.noreply.github.com>
2021-12-14 13:20:18 +0000

    project initialized

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, session
 
 from todo_app.flask_config import Config
 
@@ -8,4 +8,4 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template("index.html")
