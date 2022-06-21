8fbe46b649344cae97162140b34c230b8a3a94e1
lacattano <louis.cattano@lv.co.uk>
2022-06-21 15:35:02 +0000

    added render template

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
+from flask import render_template
 from todo_app.flask_config import Config
-
+from todo_app.data.session_items import get_items
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
