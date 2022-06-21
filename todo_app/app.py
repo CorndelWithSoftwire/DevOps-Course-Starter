06ac4c5db07f0d2b6c6e0be73e327b00a9021cc1
Alex Jones <alexander.jones@softwire.com>
2022-06-21 15:43:49 +0000

    Add newline

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items
+
 app = Flask(__name__)
 app.config.from_object(Config())
 
