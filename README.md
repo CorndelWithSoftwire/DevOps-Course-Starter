9544661dcf0fdba1bb97564a09cda8a5d36192ac
Nas <94843416+Nasirhus@users.noreply.github.com>
2023-11-08 10:35:13 +0000

    Shows to do items code

--- README.md
+++ README.md
@@ -52,3 +52,6 @@ You should see output similar to the following:
  * Debugger PIN: 226-556-590
 ```
 Now visit [`http://localhost:5000/`](http://localhost:5000/) in your web browser to view the app.
+
+```git remote -v ```
+tells us / lists our remotes
--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from todo_app.flask_config import Config
+
+from todo_app.data.session_items import get_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +10,5 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html', html_items = items)
