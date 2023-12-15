7f3726ba52d1d07bc496bbdebecad6713f344a79
Nas <94843416+Nasirhus@users.noreply.github.com>
2023-12-15 14:56:44 +0000

    Completed M1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 
 from todo_app.flask_config import Config
 
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -12,3 +14,11 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template('index.html', html_items = items)
+
+@app.route('/', methods = ["POST"])
+def new_todo():
+    todo = request.form.get("user todo")
+    item = add_item(todo)
+    return redirect('/')
+
+    
